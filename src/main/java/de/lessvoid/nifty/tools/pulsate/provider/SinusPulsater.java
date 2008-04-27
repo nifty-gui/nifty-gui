7c146071624cd1352e32bda68e05ef81195ddb5c
void256 <void256@users.sourceforge.net>
2008-04-27 22:13:26 +0000

    replaced scaleFactor with direct control of the sinus period which should make the whole thing more understandble

--- src/main/java/de/lessvoid/nifty/tools/pulsate/provider/SinusPulsater.java
+++ src/main/java/de/lessvoid/nifty/tools/pulsate/provider/SinusPulsater.java
@@ -11,9 +11,14 @@ import de.lessvoid.nifty.tools.pulsate.PulsateProvider;
 public class SinusPulsater implements PulsateProvider {
 
   /**
-   * scale factor for the sinus.
+   * constant to half things.
    */
-  private float scaleFactor;
+  private static final float HALF = 0.5f;
+
+  /**
+   * period for one full cycle in ms.
+   */
+  private float period;
 
   /**
    * start time.
@@ -21,20 +26,44 @@ public class SinusPulsater implements PulsateProvider {
   private float startTime;
 
   /**
+   * cycle (true) or one shot (false).
+   */
+  private boolean cycle = true;
+
+  /**
    * Initialize the Pulsater.
    * @param parameter the parameters
    */
   public void initialize(final Properties parameter) {
-    this.scaleFactor = Float.parseFloat(parameter.getProperty("scaleFactor", "0.005"));
+    this.period = Float.parseFloat(parameter.getProperty("period", "1000"));
+    this.cycle = "true".equals(parameter.getProperty("cycle", "true").toLowerCase());
   }
 
   /**
    * Get current value for the given time.
    * @param msTime current time
-   * @return the pulsate value in [0,1] intervall
+   * @return the pulsate value in [0,1] interval
    */
   public float getValue(final long msTime) {
-    return (float) Math.sin((startTime - msTime) * scaleFactor);
+    float t = msTime - startTime;
+    if (cycle) {
+      return getSinusValue(t);
+    } else {
+      if (t > period * HALF) {
+        return 1.0f;
+      } else {
+        return getSinusValue(t);
+      }
+    }
+  }
+
+  /**
+   * get scaled sinus.
+   * @param x input
+   * @return sinus
+   */
+  private float getSinusValue(final float x) {
+    return (float) Math.abs(Math.sin((Math.PI * x / period)));
   }
 
   /**
