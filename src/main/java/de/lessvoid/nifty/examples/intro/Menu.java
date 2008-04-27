9f9c5804b52ae5daedcba2fcda67694fdfe8fc2d
void256 <void256@users.sourceforge.net>
2008-04-27 22:15:49 +0000

    starting to consolidate all examples into one demo

---
+++ src/main/java/de/lessvoid/nifty/examples/intro/Menu.java
@@ -0,0 +1,49 @@
+package de.lessvoid.nifty.examples.intro;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+/**
+ * Menu.
+ * @author void
+ */
+public class Menu implements ScreenController {
+
+  /**
+   * the nifty instance.
+   */
+  private Nifty nifty;
+
+  /**
+   * 
+   */
+  public void bind(Nifty newNifty, Screen screen) {
+    this.nifty = newNifty;
+  }
+
+  /**
+   * on start screen.
+   */
+  public final void onStartScreen() {
+  }
+
+  /**
+   * just goto the next screen.
+   */
+  public final void onStartInteractive() {
+  }
+
+  /**
+   * on end screen.
+   */
+  public final void onEndScreen() {
+  }
+  
+  /**
+   * quit.
+   */
+  public void quit() {
+    nifty.exit();
+  }
+}
