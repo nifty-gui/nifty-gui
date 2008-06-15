71e56a91a3f5e24fdf0b907a5711168c678c5ca8
void256 <void256@users.sourceforge.net>
2008-06-15 16:23:30 +0000

    ButtonControl added

---
+++ src/main/java/de/lessvoid/nifty/controls/button/ButtonControl.java
@@ -0,0 +1,53 @@
+package de.lessvoid.nifty.controls.button;
+
+import java.util.Properties;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.Controller;
+import de.lessvoid.nifty.elements.ControllerEventListener;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.input.NiftyInputEvent;
+import de.lessvoid.nifty.screen.Screen;
+
+/**
+ * ButtonControl.
+ * @author void
+ */
+public class ButtonControl implements Controller {
+
+  /**
+   * bind.
+   * @param nifty nifty
+   * @param screen screen
+   * @param element element
+   * @param parameter parameter
+   * @param listener listener
+   */
+  public void bind(
+      final Nifty nifty,
+      final Screen screen,
+      final Element element,
+      final Properties parameter,
+      final ControllerEventListener listener) {
+  }
+
+  /**
+   * inputEvent.
+   * @param inputEvent inputEvent
+   */
+  public void inputEvent(final NiftyInputEvent inputEvent) {
+  }
+
+  /**
+   * onFocus.
+   * @param getFocus getFocus
+   */
+  public void onFocus(final boolean getFocus) {
+  }
+
+  /**
+   * onStartScreen.
+   */
+  public void onStartScreen() {
+  }
+}
