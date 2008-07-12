f3ca1c342305d71d58342d0d78a345f827c767bd
void256 <void256@users.sourceforge.net>
2008-07-12 18:25:59 +0000

    new onHover effect added: Focus ... this simple effects just does a setFocus() when an element gets an onHover effect ... nifty stuff :)

---
+++ src/main/java/de/lessvoid/nifty/effects/hover/Focus.java
@@ -0,0 +1,38 @@
+package de.lessvoid.nifty.effects.hover;
+
+import java.util.Properties;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.render.NiftyRenderEngine;
+
+/**
+ * focus on hover.
+ * @author void
+ */
+public class Focus implements HoverEffectImpl {
+
+  /**
+   * initialize.
+   * @param nifty Nifty
+   * @param element Element
+   * @param parameter Parameter
+   */
+  public void initialize(final Nifty nifty, final Element element, final Properties parameter) {
+  }
+
+  /**
+   * execute the effect.
+   * @param element the Element
+   * @param normalizedTime TimeInterpolator to use
+   * @param normalizedFalloff falloff value
+   * @param r RenderDevice to use
+   */
+  public void execute(
+      final Element element,
+      final float normalizedTime,
+      final float normalizedFalloff,
+      final NiftyRenderEngine r) {
+    element.setFocus();
+  }
+}
