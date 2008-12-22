c56273c378d6f41aefcdf4f9c903255ae4208077
void256 <void256@users.sourceforge.net>
2008-12-22 02:37:16 +0000

    order of effects is not important anymore, you can mix them now

--- src/main/java/de/lessvoid/nifty/loader/xpp3/processor/EffectsTypeProcessor.java
+++ src/main/java/de/lessvoid/nifty/loader/xpp3/processor/EffectsTypeProcessor.java
@@ -3,6 +3,7 @@ package de.lessvoid.nifty.loader.xpp3.processor;
 
 import de.lessvoid.nifty.effects.EffectEventId;
 import de.lessvoid.nifty.loader.xpp3.Attributes;
+import de.lessvoid.nifty.loader.xpp3.SubstitutionGroup;
 import de.lessvoid.nifty.loader.xpp3.XmlParser;
 import de.lessvoid.nifty.loader.xpp3.elements.EffectsType;
 import de.lessvoid.nifty.loader.xpp3.elements.ElementType;
@@ -49,13 +50,16 @@ public class EffectsTypeProcessor implements XmlElementProcessor {
   public void process(final XmlParser xmlParser, final Attributes attributes) throws Exception {
     EffectsType effectsType = new EffectsType();
     xmlParser.nextTag();
-    xmlParser.zeroOrMore("onStartScreen", new EffectTypeProcessor(effectsType, EffectEventId.onStartScreen));
-    xmlParser.zeroOrMore("onEndScreen", new EffectTypeProcessor(effectsType, EffectEventId.onEndScreen));
-    xmlParser.zeroOrMore("onHover", new EffectTypeProcessor(effectsType, EffectEventId.onHover));
-    xmlParser.zeroOrMore("onClick", new EffectTypeProcessor(effectsType, EffectEventId.onClick));
-    xmlParser.zeroOrMore("onFocus", new EffectTypeProcessor(effectsType, EffectEventId.onFocus));
-    xmlParser.zeroOrMore("onActive", new EffectTypeProcessor(effectsType, EffectEventId.onActive));
-    xmlParser.zeroOrMore("onCustom", new EffectTypeProcessor(effectsType, EffectEventId.onCustom));
+    xmlParser.zeroOrMore(
+        new SubstitutionGroup().
+          add("onStartScreen", new EffectTypeProcessor(effectsType, EffectEventId.onStartScreen)).
+          add("onEndScreen", new EffectTypeProcessor(effectsType, EffectEventId.onEndScreen)).
+          add("onHover", new EffectTypeProcessor(effectsType, EffectEventId.onHover)).
+          add("onClick", new EffectTypeProcessor(effectsType, EffectEventId.onClick)).
+          add("onFocus", new EffectTypeProcessor(effectsType, EffectEventId.onFocus)).
+          add("onActive", new EffectTypeProcessor(effectsType, EffectEventId.onActive)).
+          add("onCustom", new EffectTypeProcessor(effectsType, EffectEventId.onCustom))
+          );
 
     if (element != null) {
       element.setEffects(effectsType);
