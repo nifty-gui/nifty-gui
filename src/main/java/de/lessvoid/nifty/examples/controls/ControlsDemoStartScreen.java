4e0b551e74aaa79171d8056216308295530884e7
void256 <void256@users.sourceforge.net>
2008-12-22 14:23:10 +0000

    on change mechanism added to the drop down control

--- src/main/java/de/lessvoid/nifty/examples/controls/ControlsDemoStartScreen.java
+++ src/main/java/de/lessvoid/nifty/examples/controls/ControlsDemoStartScreen.java
@@ -2,10 +2,11 @@ package de.lessvoid.nifty.examples.controls;
 
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.controls.dropdown.DropDownControl;
+import de.lessvoid.nifty.controls.dropdown.DropDownControlNotify;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 
-public class ControlsDemoStartScreen implements ScreenController {
+public class ControlsDemoStartScreen implements ScreenController, DropDownControlNotify {
   private Nifty nifty;
   private Screen screen;
 
@@ -13,12 +14,14 @@ public class ControlsDemoStartScreen implements ScreenController {
     screen = newScreen;
     nifty = newNifty;
     DropDownControl dropDown1 = findDropDownControl("dropDown1");
+    dropDown1.addNotify(this);
     dropDown1.addItem("Nifty GUI");
     dropDown1.addItem("Slick2d");
     dropDown1.addItem("Lwjgl");
     dropDown1.setSelectedItemIdx(0);
 
     DropDownControl dropDown2 = findDropDownControl("dropDown2");
+    dropDown2.addNotify(this);
     dropDown2.addItem("rocks!");
     dropDown2.addItem("rules!");
     dropDown2.addItem("kicks ass!");
@@ -39,11 +42,18 @@ public class ControlsDemoStartScreen implements ScreenController {
   public void onEndScreen() {
   }
 
-  public final void back() {
+  public void back() {
     DropDownControl dropDown1 = findDropDownControl("dropDown1");
     System.out.println(dropDown1.getSelectedItemIdx() + ":" + dropDown1.getSelectedItem());
     DropDownControl dropDown2 = findDropDownControl("dropDown2");
     System.out.println(dropDown2.getSelectedItemIdx() + ":" + dropDown2.getSelectedItem());
     nifty.fromXml("all/intro.xml", "menu");
+  }
+
+  public void dropDownSelectionChanged(final DropDownControl dropDownControl) {
+    System.out.println(
+        "changed selection on [" + dropDownControl.toString() + "]"
+        + " to [" + dropDownControl.getSelectedItemIdx() + "]"
+        + " = [" + dropDownControl.getSelectedItem() + "]");
   }
 }
