6c1e405365460b5a43c7c943784d7920897151ee
void256 <void256@users.sourceforge.net>
2008-12-31 14:38:43 +0000

    <menu> and <menuItem> removed (you can do the same with normal elements now) TypeContext now responsible to create elements onHover-effects now responsible for falloff (optional) overlay effects added

--- src/main/java/de/lessvoid/nifty/loader/xpp3/processor/helper/ProcessorHelper.java
+++ src/main/java/de/lessvoid/nifty/loader/xpp3/processor/helper/ProcessorHelper.java
@@ -1,9 +1,7 @@
 package de.lessvoid.nifty.loader.xpp3.processor.helper;
 
-import de.lessvoid.nifty.loader.xpp3.Attributes;
 import de.lessvoid.nifty.loader.xpp3.SubstitutionGroup;
 import de.lessvoid.nifty.loader.xpp3.XmlParser;
-import de.lessvoid.nifty.loader.xpp3.elements.AttributesType;
 import de.lessvoid.nifty.loader.xpp3.elements.ElementType;
 import de.lessvoid.nifty.loader.xpp3.processor.ControlTypeProcessor;
 import de.lessvoid.nifty.loader.xpp3.processor.EffectsTypeProcessor;
@@ -11,7 +9,6 @@ import de.lessvoid.nifty.loader.xpp3.processor.HoverTypeProcessor;
 import de.lessvoid.nifty.loader.xpp3.processor.ImageTypeProcessor;
 import de.lessvoid.nifty.loader.xpp3.processor.InteractTypeProcessor;
 import de.lessvoid.nifty.loader.xpp3.processor.LabelTypeProcessor;
-import de.lessvoid.nifty.loader.xpp3.processor.MenuTypeProcessor;
 import de.lessvoid.nifty.loader.xpp3.processor.PanelTypeProcessor;
 import de.lessvoid.nifty.loader.xpp3.processor.TextTypeProcessor;
 
@@ -33,21 +30,15 @@ public final class ProcessorHelper {
    * process element attributes.
    * @param xmlParser xmlParser
    * @param elementType elementType
-   * @param attributes attributes
    * @param typeContext typeContext
    * @throws Exception exception
    */
   public static void processElement(
       final XmlParser xmlParser,
       final ElementType elementType,
-      final Attributes attributes,
       final TypeContext typeContext) throws Exception {
-    AttributesType attributesType = new AttributesType(attributes);
-    elementType.setAttributes(attributesType);
-
     xmlParser.nextTag();
     xmlParser.optional("interact", new InteractTypeProcessor(elementType));
-    xmlParser.optional("hover", new HoverTypeProcessor(elementType));
     xmlParser.optional("effect", new EffectsTypeProcessor(elementType));
     xmlParser.zeroOrMore(
           new SubstitutionGroup().
@@ -55,7 +46,6 @@ public final class ProcessorHelper {
             add("text", new TextTypeProcessor(typeContext, elementType)).
             add("label", new LabelTypeProcessor(typeContext, elementType)).
             add("image", new ImageTypeProcessor(typeContext, elementType)).
-            add("menu", new MenuTypeProcessor(typeContext, elementType)).
             add("control", new ControlTypeProcessor(typeContext, elementType))
             );
   }
--- src/main/java/de/lessvoid/nifty/loader/xpp3/processor/helper/TypeContext.java
+++ src/main/java/de/lessvoid/nifty/loader/xpp3/processor/helper/TypeContext.java
@@ -3,8 +3,16 @@ package de.lessvoid.nifty.loader.xpp3.processor.helper;
 import java.util.Map;
 
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.loader.xpp3.Attributes;
+import de.lessvoid.nifty.loader.xpp3.elements.ControlType;
+import de.lessvoid.nifty.loader.xpp3.elements.ImageType;
+import de.lessvoid.nifty.loader.xpp3.elements.LabelType;
+import de.lessvoid.nifty.loader.xpp3.elements.LayerType;
+import de.lessvoid.nifty.loader.xpp3.elements.PanelType;
+import de.lessvoid.nifty.loader.xpp3.elements.PopupType;
 import de.lessvoid.nifty.loader.xpp3.elements.RegisterControlDefinitionType;
 import de.lessvoid.nifty.loader.xpp3.elements.RegisterEffectType;
+import de.lessvoid.nifty.loader.xpp3.elements.TextType;
 import de.lessvoid.nifty.loader.xpp3.elements.helper.StyleHandler;
 import de.lessvoid.nifty.tools.TimeProvider;
 
@@ -32,5 +40,50 @@ public class TypeContext {
     this.registeredEffects = registeredEffects;
     this.registeredControls = registeredControls;
     this.time = time;
+  }
+
+  public LabelType createLabelType(final String textParam) {
+    Attributes attributes = new Attributes();
+    attributes.set("text", textParam);
+    return createLabelType(attributes);
+  }
+
+  public LabelType createLabelType(final Attributes attributes) {
+    if (!attributes.isSet("style")) {
+      attributes.set("style", "nifty-label");
+    }
+    return new LabelType(this, attributes, attributes.get("text"));
+  }
+
+  public ControlType createControlType(final Attributes attributes) {
+    ControlType controlType = new ControlType(this, attributes, attributes.get("name"));
+    if (attributes.isSet("onChange")) {
+      controlType.setOnChange(attributes.get("onChange"));
+    }
+    return controlType;
+  }
+
+  public ImageType createImageType(final Attributes attributes) {
+    ImageType imageType = new ImageType(this, attributes, attributes.get("filename"));
+    if (attributes.isSet("filter")) {
+      imageType.setFilter(attributes.getAsBoolean("filer"));
+    }
+    return imageType;
+  }
+
+  public PanelType createPanelType(final Attributes attributes) {
+    return new PanelType(this, attributes);
+  }
+
+  public LayerType createLayerType(final Attributes attributes) {
+    return new LayerType(this, attributes);
+  }
+
+  public PopupType createPopupType(final Attributes attributes) {
+    return new PopupType(this, attributes, attributes.get("controller"));
+  }
+
+  public TextType createTextType(final Attributes attributes) {
+    return new TextType(this, attributes, attributes.get("text"));
   }
 }
