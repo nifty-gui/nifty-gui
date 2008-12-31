b335a0e99bb030003fb99a839eb0763294b4782a
void256 <void256@users.sourceforge.net>
2008-12-31 14:40:44 +0000

    DefaultController added

---
+++ src/main/java/de/lessvoid/nifty/controls/DefaultController.java
@@ -0,0 +1,24 @@
+package de.lessvoid.nifty.controls;
+
+import java.util.Properties;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.elements.ControllerEventListener;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.input.NiftyInputEvent;
+import de.lessvoid.nifty.loader.xpp3.Attributes;
+import de.lessvoid.nifty.screen.Screen;
+
+public class DefaultController implements Controller {
+  public void bind(Nifty nifty, Element element, Properties parameter, ControllerEventListener listener, Attributes controlDefinitionAttributes) {
+  }
+  
+  public void inputEvent(NiftyInputEvent inputEvent) {
+  }
+  
+  public void onFocus(boolean getFocus) {
+  }
+  
+  public void onStartScreen(Screen screen) {
+  }
+}
