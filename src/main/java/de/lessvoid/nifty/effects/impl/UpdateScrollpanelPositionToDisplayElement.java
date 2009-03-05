c3dbec3558928b19dba7fcbd944f0f64d0e6c850
void256 <void256@users.sourceforge.net>
2009-03-05 05:27:15 +0000

    moved standard control classes from nifty into nifty-default-controls project so that the control definition (xml) and the implementation (java) are stored together which makes actually more sense

---
+++ src/main/java/de/lessvoid/nifty/effects/impl/UpdateScrollpanelPositionToDisplayElement.java
@@ -0,0 +1,56 @@
+package de.lessvoid.nifty.effects.impl;
+
+
+import java.util.Properties;
+import java.util.logging.Logger;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.scrollbar.VerticalScrollbar;
+import de.lessvoid.nifty.effects.EffectImpl;
+import de.lessvoid.nifty.effects.Falloff;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.render.NiftyRenderEngine;
+
+public class UpdateScrollpanelPositionToDisplayElement implements EffectImpl {
+  private Logger log = Logger.getLogger(UpdateScrollpanelPositionToDisplayElement.class.getName());
+  private Element targetElement;
+
+  public void activate(final Nifty nifty, final Element elementParameter, final Properties parameter) {
+    String target = parameter.getProperty("target");
+    if (target != null) {
+      targetElement = nifty.getCurrentScreen().findElementByName(target);
+    }
+  }
+
+  public void execute(
+      final Element element,
+      final float normalizedTime,
+      final Falloff falloff,
+      final NiftyRenderEngine r) {
+    if (targetElement != null) {
+      VerticalScrollbar verticalScrollbar = targetElement.findControl("nifty-internal-vertical-scrollbar", VerticalScrollbar.class);
+
+      int minY = (int)verticalScrollbar.getCurrentValue();
+      int maxY = (int)verticalScrollbar.getCurrentValue() + (int)verticalScrollbar.getViewMaxValue();
+
+      int currentMinY = element.getY() - targetElement.getY() + (int)verticalScrollbar.getCurrentValue();
+      int currentMaxY = element.getY() - targetElement.getY() + element.getHeight() + (int)verticalScrollbar.getCurrentValue();
+
+      // below?
+      int delta = -1;
+      if (currentMinY >= maxY || (currentMinY <= maxY && currentMaxY >= maxY)) {
+        // scroll down
+        delta = currentMaxY - maxY;
+        verticalScrollbar.setCurrentValue(minY + delta);
+      } else if (currentMaxY <= minY || (currentMinY <= minY && currentMaxY >= minY)) {
+        // scroll up
+        delta = minY - currentMinY;
+        verticalScrollbar.setCurrentValue(minY - delta);
+      }
+      log.fine(minY + ":" + maxY + " - " + currentMinY + ":" + currentMaxY + " - " + delta);
+    }
+  }
+
+  public void deactivate() {
+  }
+}
