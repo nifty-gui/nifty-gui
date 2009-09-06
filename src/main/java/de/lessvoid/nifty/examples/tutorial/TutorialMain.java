828221ce5f87a93b92825c88d0e1d1b69fca0c93
void256 <void256@users.sourceforge.net>
2009-09-06 19:57:41 +0000

    new/old tutorial added

---
+++ src/main/java/de/lessvoid/nifty/examples/tutorial/TutorialMain.java
@@ -0,0 +1,32 @@
+package de.lessvoid.nifty.examples.tutorial;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.examples.LwjglInitHelper;
+import de.lessvoid.nifty.lwjglslick.render.RenderDeviceLwjgl;
+import de.lessvoid.nifty.lwjglslick.sound.SlickSoundDevice;
+import de.lessvoid.nifty.sound.SoundSystem;
+import de.lessvoid.nifty.tools.TimeProvider;
+
+public final class TutorialMain {
+
+  private TutorialMain() {
+  }
+
+  public static void main(final String[] args) {
+    if (!LwjglInitHelper.initSubSystems("Nifty Tutorial")) {
+      System.exit(0);
+    }
+
+    // create nifty
+    Nifty nifty = new Nifty(
+        new RenderDeviceLwjgl(),
+        new SoundSystem(new SlickSoundDevice()),
+        LwjglInitHelper.getInputSystem(),
+        new TimeProvider());
+    nifty.fromXml("tutorial/tutorial.xml", "start");
+
+    // render
+    LwjglInitHelper.renderLoop(nifty, null);
+    LwjglInitHelper.destroy();
+  }
+}
