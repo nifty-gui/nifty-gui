4221ff4e99307654266bf402e4afb8557a41e74c
void256 <void256@users.sourceforge.net>
2010-04-09 22:20:42 +0000

    @public added experimental java2d nifty renderer dependency added schemaLocation to nifty example xml files with url "http://nifty-gui.sourceforge.net/nifty.xsd" to support xml tools with schema validation cababilities

---
+++ src/main/java/de/lessvoid/nifty/examples/tutorial/TutorialMainJava2d.java
@@ -0,0 +1,68 @@
+package de.lessvoid.nifty.examples.tutorial;
+
+import java.awt.Canvas;
+import java.awt.Dimension;
+import java.awt.Frame;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.java2d.input.InputSystemAwt;
+import de.lessvoid.nifty.java2d.renderer.RenderDeviceJava2dImpl;
+import de.lessvoid.nifty.lwjglslick.sound.SlickSoundDevice;
+import de.lessvoid.nifty.sound.SoundSystem;
+import de.lessvoid.nifty.tools.TimeProvider;
+
+public final class TutorialMainJava2d {
+
+  public static void main(final String[] args) {
+    InputSystemAwt inputSystem = new InputSystemAwt();
+
+    Dimension dimension = new Dimension(1024, 768);
+
+    final Canvas canvas = new Canvas();
+    canvas.addMouseMotionListener(inputSystem);
+    canvas.addMouseListener(inputSystem);
+
+    Frame f = new Frame("Nifty Java 2d");
+    f.addWindowListener(new WindowAdapter() {
+      public void windowClosing(WindowEvent e) {
+        System.exit(0);
+      }
+
+      public void windowDeiconified(WindowEvent e) {
+      }
+
+      public void windowIconified(WindowEvent e) {
+      }
+    });
+    
+    f.add(canvas);
+    f.pack();
+    f.setSize(dimension);
+    f.setVisible(true);
+    // f.setIgnoreRepaint(true);
+    // canvas.setIgnoreRepaint(true);
+
+    RenderDeviceJava2dImpl renderDevice = new RenderDeviceJava2dImpl(canvas);
+
+    Nifty nifty = new Nifty(renderDevice, new SoundSystem(new SlickSoundDevice()), inputSystem, new TimeProvider());
+    nifty.fromXml("tutorial/tutorial.xml", "start");
+
+    boolean done = false;
+    long time = System.currentTimeMillis();
+    long frames = 0;
+    while (!done) {
+      done = nifty.render(false);
+      frames++;
+
+      long diff = System.currentTimeMillis() - time;
+      if (diff >= 1000) {
+        time += diff;
+        System.out.println("fps: " + frames);
+        frames = 0;
+      }
+    }
+
+  }
+}
