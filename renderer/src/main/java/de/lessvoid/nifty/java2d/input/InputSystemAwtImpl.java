96e561fe893b225e276baf2f293ecf5b8504bcd9
void256 <void256@users.sourceforge.net>
2010-05-10 11:52:54 +0000

    @public Changed the InputSystem for latest Nifty 1.2 changes

--- renderer/src/main/java/de/lessvoid/nifty/java2d/input/InputSystemAwtImpl.java
+++ renderer/src/main/java/de/lessvoid/nifty/java2d/input/InputSystemAwtImpl.java
@@ -3,10 +3,9 @@ package de.lessvoid.nifty.java2d.input;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
 import java.awt.event.MouseMotionListener;
-import java.util.LinkedList;
-import java.util.List;
 import java.util.concurrent.ConcurrentLinkedQueue;
 
+import de.lessvoid.nifty.NiftyInputConsumer;
 import de.lessvoid.nifty.input.keyboard.KeyboardInputEvent;
 import de.lessvoid.nifty.input.mouse.MouseInputEvent;
 import de.lessvoid.nifty.spi.input.InputSystem;
@@ -15,27 +14,21 @@ public class InputSystemAwtImpl implements InputSystem, MouseMotionListener,
 		MouseListener {
 
 	private ConcurrentLinkedQueue<MouseInputEvent> mouseEvents = new ConcurrentLinkedQueue<MouseInputEvent>();
-
 	private ConcurrentLinkedQueue<KeyboardInputEvent> keyboardEvents = new ConcurrentLinkedQueue<KeyboardInputEvent>();
 
-	public List<MouseInputEvent> getMouseEvents() {
-		LinkedList<MouseInputEvent> list = new LinkedList<MouseInputEvent>();
-		MouseInputEvent event = mouseEvents.poll();
-		while (event != null) {
-			list.add(event);
-			event = mouseEvents.poll();
+  @Override
+  public void forwardEvents(final NiftyInputConsumer inputEventConsumer) {
+		MouseInputEvent mouseEvent = mouseEvents.poll();
+		while (mouseEvent != null) {
+		  inputEventConsumer.processMouseEvent(mouseEvent);
+			mouseEvent = mouseEvents.poll();
 		}
-		return list;
-	}
 
-	public List<KeyboardInputEvent> getKeyboardEvents() {
-		LinkedList<KeyboardInputEvent> list = new LinkedList<KeyboardInputEvent>();
-		KeyboardInputEvent event = keyboardEvents.poll();
-		while (event != null) {
-			list.add(event);
-			event = keyboardEvents.poll();
+		KeyboardInputEvent keyEvent = keyboardEvents.poll();
+		while (keyEvent != null) {
+		  inputEventConsumer.processKeyboardEvent(keyEvent);
+		  keyEvent = keyboardEvents.poll();
 		}
-		return list;
 	}
 
 	@Override
@@ -74,5 +67,4 @@ public class InputSystemAwtImpl implements InputSystem, MouseMotionListener,
 		mouseEvents.add(new MouseInputEvent(mouseEvent.getX(), mouseEvent
 				.getY(), mouseEvent.getButton() == MouseEvent.BUTTON1));
 	}
-
 }
