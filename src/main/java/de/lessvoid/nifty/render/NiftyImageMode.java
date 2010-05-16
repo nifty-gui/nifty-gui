7403d7fda828ec9939c869a472da15fddab77b2c
void256 <void256@users.sourceforge.net>
2010-05-16 23:48:57 +0000

    @public new feature: imageMode="repeat:0,0,100,100": repeat image mode added: you can use this to repeat a pattern across a bigger area.

--- src/main/java/de/lessvoid/nifty/render/NiftyImageMode.java
+++ src/main/java/de/lessvoid/nifty/render/NiftyImageMode.java
@@ -13,39 +13,25 @@ import de.lessvoid.nifty.tools.Color;
  * - "normal" (render the given image to the given image size)
  * - "subImage:" (render the given subImage of the image to the given size)
  * - "resize:" (special mode that allows scaling of bitmaps with a predefined schema)
+ * - "repeat:" (special mode that repeating of texture)
  *
  * @author void
  */
 public class NiftyImageMode {
-  /**
-   * logger.
-   */
   private static Logger log = Logger.getLogger(NiftyImageMode.class.getName());
 
-  /**
-   * normal/default string.
-   */
   private static final String NORMAL_STRING = "normal";
-
-  /**
-   * resize string.
-   */
   private static final String RESIZE_STRING = "resize:";
-
-  /**
-   * subImage string.
-   */
   private static final String SUB_IMAGE_STRING = "subImage:";
+  private static final String REPEAT_STRING = "repeat:";
 
-  /**
-   * subImage args count.
-   */
   private static final int SUBIMAGE_ARGS_COUNT = 4;
+  private static final int REPEAT_ARGS_COUNT = 4;
 
   /**
    * supported modes.
    */
-  private enum Mode { NORMAL, SUBIMAGE, RESIZE };
+  private enum Mode { NORMAL, SUBIMAGE, RESIZE, REPEAT };
 
   /**
    * actual mode.
@@ -56,6 +42,11 @@ public class NiftyImageMode {
    * sub image box to scale.
    */
   private Box subImageBox;
+
+  /**
+   * repeat box to scale.
+   */
+  private Box repeatBox;
 
   /**
    * resize string.
@@ -116,6 +107,11 @@ public class NiftyImageMode {
     this.resizeString = newResizeString;
   }
 
+  public NiftyImageMode(final Box box, final boolean b) {
+    this.mode = Mode.REPEAT;
+    this.repeatBox = box;
+  }
+
   /**
    * create a RenderImageMode from the given String.
    * @param imageMode imageMode String
@@ -128,6 +124,8 @@ public class NiftyImageMode {
       return handleSubImage(imageMode);
     } else if (imageMode.startsWith(RESIZE_STRING)) {
       return handleResize(imageMode);
+    } else if (imageMode.startsWith(REPEAT_STRING)) {
+      return handleRepeat(imageMode);
     } else {
       return NiftyImageMode.normal();
     }
@@ -159,6 +157,34 @@ public class NiftyImageMode {
     int w = Integer.valueOf(args[index++]);
     int h = Integer.valueOf(args[index++]);
     return NiftyImageMode.subImage(x, y, w, h);
+  }
+
+  /**
+   * handle repeat
+   * @param imageMode image mode
+   * @return RenderImageMode
+   */
+  private static NiftyImageMode handleRepeat(final String imageMode) {
+    String parameters = imageMode.replaceFirst(REPEAT_STRING, "");
+    if (parameters == null || parameters.length() == 0) {
+      log.warning("trying to parse imageMode [" + imageMode + "] but missing repeat definition! using default RenderImageMode normal.");
+      return NiftyImageMode.normal();
+    }
+    String[] args = parameters.split(",");
+    if (args.length != REPEAT_ARGS_COUNT) {
+      log.warning("expecting exactly 4 parameters but got only " + args.length + " using default RenderImageMode normal.");
+      return NiftyImageMode.normal();
+    }
+    int index = 0;
+    int x = Integer.valueOf(args[index++]);
+    int y = Integer.valueOf(args[index++]);
+    int w = Integer.valueOf(args[index++]);
+    int h = Integer.valueOf(args[index++]);
+    return NiftyImageMode.repeat(x, y, w, h);
+  }
+
+  private static NiftyImageMode repeat(final int x, final int y, final int w, final int h) {
+    return new NiftyImageMode(new Box(x, y, w, h), true);
   }
 
   /**
@@ -199,22 +225,67 @@ public class NiftyImageMode {
     int centerX = x + width/2;
     int centerY = y + height/2;
 
-    if (mode == Mode.NORMAL) {
-      renderDevice.renderImage(renderImage, x, y, width, height, color, scale);
-    } else {
-      if (mode == Mode.SUBIMAGE) {
-        renderDevice.renderImage(
-            renderImage,
-            x, y, width, height,
-            subImageBox.getX(), subImageBox.getY(), subImageBox.getWidth(), subImageBox.getHeight(),
-            color,
-            scale,
-            centerX,
-            centerY);
-      } else if (mode == Mode.RESIZE) {
-        ResizeHelper resizeHelper = new ResizeHelper(renderDevice, renderImage, this.resizeString);
-        resizeHelper.performRender(x, y, width, height, color, scale, centerX, centerY);
+    if (mode == Mode.SUBIMAGE) {
+      renderDevice.renderImage(
+          renderImage,
+          x, y, width, height,
+          subImageBox.getX(), subImageBox.getY(), subImageBox.getWidth(), subImageBox.getHeight(),
+          color,
+          scale,
+          centerX,
+          centerY);
+    } else if (mode == Mode.REPEAT) {
+      int countX = width / repeatBox.getWidth();
+      int countY = height / repeatBox.getHeight();
+
+      for (int j=0; j<countY; j++) {
+        renderRepeatLine(renderDevice, renderImage, x, y, width, color, scale, centerX, centerY, countX, j, repeatBox.getHeight());
       }
+      int partly = height % repeatBox.getHeight();
+      if (partly != 0) {
+        renderRepeatLine(renderDevice, renderImage, x, y, width, color, scale, centerX, centerY, countX, countY, partly);
+      }
+    } else if (mode == Mode.RESIZE) {
+      ResizeHelper resizeHelper = new ResizeHelper(renderDevice, renderImage, this.resizeString);
+      resizeHelper.performRender(x, y, width, height, color, scale, centerX, centerY);
+    } else {
+      renderDevice.renderImage(renderImage, x, y, width, height, color, scale);
+    }
+  }
+
+  private void renderRepeatLine(final RenderDevice renderDevice, final RenderImage renderImage, final int x, final int y, final int width, final Color color, final float scale, int centerX, int centerY, int countX, int j, int height) {
+    for (int i=0; i<countX; i++) {
+      renderDevice.renderImage(
+          renderImage,
+          x + i*repeatBox.getWidth(),
+          y + j*repeatBox.getHeight(),
+          repeatBox.getWidth(),
+          height,
+          repeatBox.getX(),
+          repeatBox.getY(),
+          repeatBox.getWidth(),
+          height,
+          color,
+          scale,
+          centerX,
+          centerY);
+    }
+    int partlx = width % repeatBox.getWidth();
+    if (partlx != 0) {
+      renderDevice.renderImage(
+          renderImage,
+          x + width - partlx,
+          y + j*repeatBox.getHeight(),
+          partlx,
+          height,
+          repeatBox.getX(),
+          repeatBox.getY(),
+          partlx,
+          height,
+          color,
+          scale,
+          centerX,
+          centerY);
     }
   }
 }
