ad20f5ff6b1a5fc3afdd3ca4799f1512b54ceaa9
void256 <void256@users.sourceforge.net>
2010-05-21 17:31:21 +0000

    some updates to the allcontrols example

--- src/main/java/de/lessvoid/nifty/examples/allcontrols/AllControlsDemoStartScreen.java
+++ src/main/java/de/lessvoid/nifty/examples/allcontrols/AllControlsDemoStartScreen.java
@@ -2,6 +2,7 @@ package de.lessvoid.nifty.examples.allcontrols;
 
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.controls.dropdown.controller.DropDownControl;
+import de.lessvoid.nifty.controls.listbox.controller.ListBoxControl;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 
@@ -11,7 +12,18 @@ public class AllControlsDemoStartScreen implements ScreenController {
     dropDownControl.addItem("Item 1");
     dropDownControl.addItem("Item 2");
     dropDownControl.addItem("Item 3");
+    dropDownControl.addItem("Item 4");
+    dropDownControl.addItem("Item 5");
+    dropDownControl.addItem("Item 6");
     dropDownControl.setSelectedItemIdx(0);
+
+    ListBoxControl listBoxControl = screen.findControl("listBox", ListBoxControl.class);
+    listBoxControl.addItem("Item 1");
+    listBoxControl.addItem("Item 2");
+    listBoxControl.addItem("Item 3");
+    listBoxControl.addItem("Item 4");
+    listBoxControl.addItem("Item 5");
+    listBoxControl.addItem("Item 6");
   }
 
   public void onStartScreen() {
