2d5aceed6338c0277b4db9f17891a511fb55e18e
void256 <void256@users.sourceforge.net>
2010-05-31 21:43:03 +0000

    falloff test added

---
+++ src/main/java/de/lessvoid/nifty/examples/falloff/FalloffMain.java
@@ -0,0 +1,32 @@
+package de.lessvoid.nifty.examples.falloff;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.examples.LwjglInitHelper;
+import de.lessvoid.nifty.lwjglslick.render.RenderDeviceLwjgl;
+import de.lessvoid.nifty.lwjglslick.sound.SlickSoundDevice;
+import de.lessvoid.nifty.sound.SoundSystem;
+import de.lessvoid.nifty.tools.TimeProvider;
+
+public final class FalloffMain {
+
+  private FalloffMain() {
+  }
+
+  public static void main(final String[] args) {
+    if (!LwjglInitHelper.initSubSystems("Nifty General Test")) {
+      System.exit(0);
+    }
+
+    // create nifty
+    Nifty nifty = new Nifty(
+        new RenderDeviceLwjgl(),
+        new SlickSoundDevice(),
+        LwjglInitHelper.getInputSystem(),
+        new TimeProvider());
+    nifty.fromXml("falloff/falloff.xml", "start");
+
+    // render
+    LwjglInitHelper.renderLoop(nifty, null);
+    LwjglInitHelper.destroy();
+  }
+}
