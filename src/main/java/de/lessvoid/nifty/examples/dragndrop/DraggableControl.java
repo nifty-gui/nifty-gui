bdca48e6c46d239e4f5d79b4c4e8a8433f9fcb0e
void256 <void256@users.sourceforge.net>
2010-06-26 20:55:57 +0000

    @public drag and drop example patch added from zozo64

--- src/main/java/de/lessvoid/nifty/examples/dragndrop/DraggableControl.java
+++ src/main/java/de/lessvoid/nifty/examples/dragndrop/DraggableControl.java
@@ -22,29 +22,45 @@ public class DraggableControl implements Controller {
   private Element draggable;
   private Element originalParent;
   private Element popup;
+  private Element handle;
+  private boolean revert;
+  private boolean drop;
+  
   private boolean dragged = false;
   private int originalPositionX;
   private int originalPositionY;
+  private SizeValue originalConstraintX;
+  private SizeValue originalConstraintY;
   private int dragStartX;
   private int dragStartY;
-
+  
   @Override
   public void bind(Nifty nifty, Screen screen, Element element, Properties parameter, ControllerEventListener listener, Attributes controlDefinitionAttributes) {
     this.nifty = nifty;
     this.screen = screen;
     draggable = element;
+    
+    String handleId= controlDefinitionAttributes.get("handle");
+    handle = draggable.findElementByName(handleId);
+    if (handle == null) 
+      handle = draggable;
+    revert = controlDefinitionAttributes.getAsBoolean("revert", true);
+    drop = controlDefinitionAttributes.getAsBoolean("drop", true);
   }
 
   public void dragStart(final int mouseX, final int mouseY) {
     System.out.println("dragStart(" + dragged + ")");
     if (dragged)
       return;
+    if (!handle.isMouseInsideElement(mouseX, mouseY))
+      return;
     dragged = true;
     logger.fine(String.format("dragStart(mouseX=%s, mouseY=%s), draggable.id=%s, draggable.x=%s, draggable.y=%s", mouseX, mouseY, draggable.getId(), draggable.getX(), draggable.getY()));
     originalParent = draggable.getParent();
-    System.out.println("dragStart: " + originalParent.toString());
     originalPositionX = draggable.getX();
     originalPositionY = draggable.getY();
+    originalConstraintX = draggable.getConstraintX();
+    originalConstraintY = draggable.getConstraintY();
     dragStartX = mouseX;
     dragStartY = mouseY;
 
@@ -52,78 +68,76 @@ public class DraggableControl implements Controller {
     draggable.markForMove(popup, new EndNotify() {
       @Override
       public void perform() {
+        draggable.setConstraintX(new SizeValue(originalPositionX + "px"));
+        draggable.setConstraintY(new SizeValue(originalPositionY + "px"));
+        // popup.layoutElements();
         nifty.showPopup(screen, POPUP, null);
-        int newPositionX = originalPositionX + mouseX - dragStartX;
-        int newPositionY = originalPositionY + mouseY - dragStartY;
-        draggable.setConstraintX(new SizeValue(newPositionX + "px"));
-        draggable.setConstraintY(new SizeValue(newPositionY + "px"));
-        popup.layoutElements();
       }
     });
   }
 
   public void drag(int mouseX, int mouseY) {
-    System.out.println("drag(" + dragged + ")");
     if (!dragged)
       return;
-    logger.fine(String.format("drag(mouseX=%s, mouseY=%s)", mouseX, mouseY));
+    
     int newPositionX = originalPositionX + mouseX - dragStartX;
     int newPositionY = originalPositionY + mouseY - dragStartY;
     draggable.setConstraintX(new SizeValue(newPositionX + "px"));
     draggable.setConstraintY(new SizeValue(newPositionY + "px"));
+    
     popup.layoutElements();
-    logger.info("popup: " + popup.getId());
-    logger.info(String.format("drag() newPos.x=%s, newPos.y=%s", newPositionX, newPositionY));
-    logger.info(String.format("drag() element.x=%s, element.y=%s", draggable.getX(), draggable.getY()));
   }
-  
+
   public void dragStop() {
     if (!dragged)
       return;
     logger.fine("dragStop()");
-    // logger.fine(String.format("dragStop(mouseX=%s, mouseY=%s)", mouseX, mouseY));
     Element droppable = findDroppableAtCurrentCoordinates();
     if (droppable == originalParent) {
-      draggable.setConstraintX(new SizeValue(originalPositionX + "px"));
-      draggable.setConstraintY(new SizeValue(originalPositionY + "px"));
-      draggable.markForMove(originalParent, new EndNotify() {
-        @Override
-        public void perform() {
-          nifty.closePopup(POPUP, new EndNotify() {
-            @Override
-            public void perform() {
-              draggable.reactivate();
-              System.out.println(screen.debugOutput());
-            }
-          });
-        }
-      });
+      if (revert) {
+        draggable.setConstraintX(originalConstraintX);
+        draggable.setConstraintY(originalConstraintY);
+      } else {
+        SizeValue newConstraintX = translateValue(originalConstraintX, draggable.getX() - originalPositionX);
+        SizeValue newConstraintY = translateValue(originalConstraintY, draggable.getY() - originalPositionY);
+        draggable.setConstraintX(new SizeValue(draggable.getX() + "px"));
+        draggable.setConstraintY(new SizeValue(draggable.getY() + "px"));
+      }
     } else {
-      draggable.setConstraintX(new SizeValue(droppable.getX() + "px"));
-      draggable.setConstraintY(new SizeValue(droppable.getY() + "px"));
-      draggable.markForMove(droppable, new EndNotify() {
-        @Override
-        public void perform() {
-          nifty.closePopup(POPUP, new EndNotify() {
-            @Override
-            public void perform() {
-              draggable.reactivate();
-              System.out.println(screen.debugOutput());
-            }
-          });
-        }
-      });
+      draggable.setConstraintX(new SizeValue("0px"));
+      draggable.setConstraintY(new SizeValue("0px"));
     }
+
+    draggable.markForMove(droppable, new EndNotify() {
+      @Override
+      public void perform() {
+        nifty.closePopup(POPUP, new EndNotify() {
+          @Override
+          public void perform() {
+            draggable.reactivate();
+          }
+        });
+      }
+    });
     dragged = false;
   }
   
+  private SizeValue translateValue(SizeValue value,
+      int movement) {
+    int oldValue = (value != null) ? value.getValueAsInt(1.0f) : 0;
+    oldValue = 0;
+    return new SizeValue(oldValue + movement +  "px");
+  }
+
   private Element findDroppableAtCurrentCoordinates() {
-    int dragAnkerX = draggable.getX() + draggable.getWidth() / 2;
-    int dragAnkerY = draggable.getY() + draggable.getHeight() / 2;
-    for (Element layer : screen.getLayerElements()) {
-      Element droppable = findDroppableAtCoordinates(layer, dragAnkerX, dragAnkerY);
-      if (droppable != null)
-        return droppable;
+    if (drop) {
+      int dragAnkerX = draggable.getX() + draggable.getWidth() / 2;
+      int dragAnkerY = draggable.getY() + draggable.getHeight() / 2;
+      for (Element layer : screen.getLayerElements()) {
+        Element droppable = findDroppableAtCoordinates(layer, dragAnkerX, dragAnkerY);
+        if (droppable != null)
+          return droppable;
+      }
     }
     return originalParent;
   }
---
+++ src/main/java/de/lessvoid/nifty/examples/dragndrop/WindowControl.java
@@ -0,0 +1,41 @@
+package de.lessvoid.nifty.examples.dragndrop;
+
+import java.util.Properties;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.Controller;
+import de.lessvoid.nifty.elements.ControllerEventListener;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.input.NiftyInputEvent;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.xml.xpp3.Attributes;
+
+public class WindowControl implements Controller {
+
+  @Override
+  public void bind(Nifty nifty, Screen screen, Element element,
+      Properties parameter, ControllerEventListener listener,
+      Attributes controlDefinitionAttributes) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void inputEvent(NiftyInputEvent inputEvent) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onFocus(boolean getFocus) {
+    // TODO Auto-generated method stub
+    
+  }
+
+  @Override
+  public void onStartScreen() {
+    // TODO Auto-generated method stub
+    
+  }
+
+}
