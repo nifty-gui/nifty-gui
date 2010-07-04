e02aed188ed263ab1d2eb9327515976d48e61a24
void256 <void256@users.sourceforge.net>
2010-07-04 12:36:58 +0000

    initial version

---
+++ src/main/java/de/lessvoid/nifty/sound/paulssoundsystem/PaulsSoundsystemSoundDevice.java
@@ -0,0 +1,49 @@
+package de.lessvoid.nifty.sound.paulssoundsystem;
+
+import paulscode.sound.SoundSystemConfig;
+import paulscode.sound.SoundSystemException;
+import paulscode.sound.codecs.CodecJOrbis;
+import paulscode.sound.codecs.CodecWav;
+import de.lessvoid.nifty.sound.SoundSystem;
+import de.lessvoid.nifty.spi.sound.SoundDevice;
+import de.lessvoid.nifty.spi.sound.SoundHandle;
+
+public class PaulsSoundsystemSoundDevice implements SoundDevice {
+  private paulscode.sound.SoundSystem soundSystem;
+  private int counter = 0;
+
+  public PaulsSoundsystemSoundDevice(final Class libraryClass, final SupportedCodec ... additionalCodecs) throws SoundSystemException {
+    SoundSystemConfig.setSoundFilesPackage("");
+    SoundSystemConfig.addLibrary(libraryClass);
+
+    // we decided that ogg and wav are build in
+    SoundSystemConfig.setCodec("ogg", CodecJOrbis.class);
+    SoundSystemConfig.setCodec("wav", CodecWav.class);
+
+    addAdditionalCodecs(libraryClass, additionalCodecs);
+    soundSystem = new paulscode.sound.SoundSystem();
+  }
+
+  private void addAdditionalCodecs(final Class libraryClass, final SupportedCodec... codecs) throws SoundSystemException {
+    if (codecs != null) {
+      for (SupportedCodec codec : codecs) {
+        SoundSystemConfig.setCodec(codec.getExtension(), codec.getCodecClass());
+      }
+    }
+  }
+
+  public SoundHandle loadSound(final SoundSystem soundSystem, final String filename) {
+    return new PaulsSoundHandle(this.soundSystem, filename);
+  }
+
+  public SoundHandle loadMusic(final SoundSystem soundSystem, final String filename) {
+    return new PaulsMusicHandle(this.soundSystem, generateId(), filename);
+  }
+
+  public void update(final int delta) {
+  }
+
+  private String generateId() {
+    return String.valueOf(counter++);
+  }
+}
