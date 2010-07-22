7caa1ae1dd261539bbef4b073d8796623d6c4827
void256 <void256@users.sourceforge.net>
2010-07-22 22:01:30 +0000

    clickable links added to the tutorial :)

--- src/main/java/de/lessvoid/nifty/examples/tutorial/screen/MainPage.java
+++ src/main/java/de/lessvoid/nifty/examples/tutorial/screen/MainPage.java
@@ -254,4 +254,24 @@ public class MainPage implements ScreenController, KeyInputHandler {
       }
     });
   }
+
+  public void openLink(final String url) {
+    if (!java.awt.Desktop.isDesktopSupported()) {
+      System.err.println("Desktop is not supported (Can't open link)");
+      return;
+    }
+
+    java.awt.Desktop desktop = java.awt.Desktop.getDesktop();
+    if (!desktop.isSupported(java.awt.Desktop.Action.BROWSE)) {
+      System.err.println("Desktop (BROWSE) is not supported (Can't open link)");
+      return;
+    }
+
+    try {
+      java.net.URI uri = new java.net.URI(url);
+      desktop.browse(uri);
+    } catch (Exception e) {
+      e.printStackTrace();
+    }
+  }
 }
