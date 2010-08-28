57e79226134b085e97464166716a3e27470b9de5
void256 <void256@users.sourceforge.net>
2010-08-28 12:56:34 +0000

    @public updated HelloNiftyBuilderExampleMain and made it really compact (a Nifty GUI in one single file! :D)

--- src/main/java/de/lessvoid/nifty/examples/helloniftybuilder/HelloNiftyBuilderExampleMain.java
+++ src/main/java/de/lessvoid/nifty/examples/helloniftybuilder/HelloNiftyBuilderExampleMain.java
@@ -1,11 +1,7 @@
 package de.lessvoid.nifty.examples.helloniftybuilder;
 
-import java.awt.Color;
-
 import de.lessvoid.nifty.Nifty;
-import de.lessvoid.nifty.builder.ElementBuilder.Align;
-import de.lessvoid.nifty.builder.ElementBuilder.LayoutType;
-import de.lessvoid.nifty.builder.ElementBuilder.VAlign;
+import de.lessvoid.nifty.builder.ImageBuilder;
 import de.lessvoid.nifty.builder.LayerBuilder;
 import de.lessvoid.nifty.builder.PanelBuilder;
 import de.lessvoid.nifty.builder.ScreenBuilder;
@@ -15,6 +11,7 @@ import de.lessvoid.nifty.controls.dynamic.attributes.ControlEffectOnHoverAttribu
 import de.lessvoid.nifty.examples.LwjglInitHelper;
 import de.lessvoid.nifty.renderer.lwjgl.render.LwjglRenderDevice;
 import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
 import de.lessvoid.nifty.sound.openal.OpenALSoundDevice;
 import de.lessvoid.nifty.tools.TimeProvider;
 
@@ -23,13 +20,11 @@ import de.lessvoid.nifty.tools.TimeProvider;
  * to dynamically create Nifty screens WITHOUT the xml.
  * @author void
  */
-public class HelloNiftyBuilderExampleMain {
-
-  private HelloNiftyBuilderExampleMain() {
-  }
+public class HelloNiftyBuilderExampleMain implements ScreenController {
+  private Nifty nifty;
 
   public static void main(final String[] args) {
-    if (!LwjglInitHelper.initSubSystems("Nifty Hello World")) {
+    if (!LwjglInitHelper.initSubSystems("Hello Nifty Builder World")) {
       System.exit(0);
     }
 
@@ -41,79 +36,73 @@ public class HelloNiftyBuilderExampleMain {
         new TimeProvider());
 
     // create a screen
-    Screen screen = new ScreenBuilder("start") {
-      {
-        controller(new HelloNiftyBuilderExampleScreenController());
+    Screen screen = new ScreenBuilder("start") {{
+      controller(new HelloNiftyBuilderExampleMain());
 
-        layer(new LayerBuilder("layer") {
-          {
-            backgroundColor("#003f");
-            childLayout(LayoutType.Center);
+      layer(new LayerBuilder("layer") {{
+        backgroundColor("#003f");
+        childLayoutCenter();
 
-            panel(new PanelBuilder() {
-              {
-                id("panel");
-                height("25%");
-                width("80%");
-                align(Align.Center);
-                valign(VAlign.Center);
-                backgroundColor("#f60f");
-                childLayout(LayoutType.Center);
-                visibleToMouse(true);
+        panel(new PanelBuilder() {{
+          id("panel");
+          childLayoutCenter();
+          height("25%");
+          width("80%");
+          alignCenter();
+          valignCenter();
+          backgroundColor("#f60f");
+          visibleToMouse();
+          interactOnClick("quit()");
+          padding("10px");
 
-                // I want this to be a callable maybe
-                interactOnClick("quit()");
+          onStartScreenEffect(new ControlEffectAttributes() {{
+            setAttribute("name", "move");
+            setAttribute("mode", "in");
+            setAttribute("direction", "top");
+            setAttribute("length", "300");
+            setAttribute("startDelay", "0");
+            setAttribute("inherit", "true");
+          }});
 
-                onStartScreenEffect(new ControlEffectAttributes() {
-                  {
-                    setAttribute("name", "move");
-                    setAttribute("mode", "in");
-                    setAttribute("direction", "top");
-                    setAttribute("length", "300");
-                    setAttribute("startDelay", "0");
-                    setAttribute("inherit", "true");
-                  }
-                });
+          onEndScreenEffect(new ControlEffectAttributes() {{
+            setAttribute("name", "move");
+            setAttribute("mode", "out");
+            setAttribute("direction", "bottom");
+            setAttribute("length", "300");
+            setAttribute("startDelay", "0");
+            setAttribute("inherit", "true");
+          }});
 
-                onEndScreenEffect(new ControlEffectAttributes() {
-                  {
-                    setAttribute("name", "move");
-                    setAttribute("mode", "out");
-                    setAttribute("direction", "bottom");
-                    setAttribute("length", "300");
-                    setAttribute("startDelay", "0");
-                    setAttribute("inherit", "true");
-                  }
-                });
+          onHoverEffect(new ControlEffectOnHoverAttributes() {{
+            setAttribute("name", "pulsate");
+            setAttribute("scaleFactor", "0.008");
+            setAttribute("startColor", "#f600");
+            setAttribute("endColor", "#ffff");
+            setAttribute("post", "true");
+          }});
 
-                onHoverEffect(new ControlEffectOnHoverAttributes() {
-                  {
-                    setAttribute("name", "pulsate");
-                    setAttribute("scaleFactor", "0.008");
-                    setAttribute("startColor", "#f600");
-                    setAttribute("endColor", "#ffff");
-                    setAttribute("post", "true");
-                  }
-                });
+          panel(new PanelBuilder() {{
+            childLayoutHorizontal();
+            alignCenter();
+            valignCenter();
+            width("100%");
 
-                text(new TextBuilder() {
-                  {
-                    id("text1");
-                    text("Hello World!!");
+            image(new ImageBuilder() {{
+              filename("nifty-logo-150x150.png");
+            }});
 
-                    font("aurulent-sans-17.fnt");
-                    color("#000f");
-                    align(Align.Center);
-                    valign(VAlign.Center);
-                  }
-                });
-
-              }
-            });
-          }
-        });
-      }
-    }.build(nifty);
+            text(new TextBuilder() {{
+              text("Hello Nifty Builder World!!!");
+              font("aurulent-sans-17.fnt");
+              color("#000f");
+              width("*");
+              alignCenter();
+              valignCenter();
+            }});
+          }});
+        }});
+      }});
+    }}.build(nifty);
 
     nifty.addScreen("start", screen);
     nifty.gotoScreen("start");
@@ -121,5 +110,25 @@ public class HelloNiftyBuilderExampleMain {
     // render
     LwjglInitHelper.renderLoop(nifty, null);
     LwjglInitHelper.destroy();
+  }
+
+  @Override
+  public void bind(final Nifty nifty, final Screen screen) {
+    System.out.println("bind()");
+    this.nifty = nifty;
+  }
+
+  @Override
+  public void onStartScreen() {
+    System.out.println("onStartScreen()");
+  }
+
+  @Override
+  public void onEndScreen() {
+    System.out.println("onEndScreen()");
+  }
+
+  public void quit() {
+    nifty.exit();
   }
 }
