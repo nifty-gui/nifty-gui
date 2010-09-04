8491f24e2469cee76ba6299eb3fc886510eb7672
void256 <void256@users.sourceforge.net>
2010-09-04 09:34:17 +0000

    @public added NiftyOverlayGameState Examples work of durandal

--- src/main/java/de/lessvoid/nifty/examples/slick/niftyoverlay/Main.java
+++ src/main/java/de/lessvoid/nifty/examples/slick/niftyoverlay/Main.java
@@ -1,9 +1,8 @@
 package de.lessvoid.nifty.examples.slick.niftyoverlay;
 
 import org.newdawn.slick.AppGameContainer;
-import org.newdawn.slick.GameContainer;
 import org.newdawn.slick.SlickException;
-import org.newdawn.slick.state.StateBasedGame;
+
 
 /**
  * This demonstrates a standard slick GameState that has been "overlayed" with
@@ -12,19 +11,14 @@ import org.newdawn.slick.state.StateBasedGame;
  * @author void
  *
  */
-public class OverlayMain extends StateBasedGame {
-  public OverlayMain(final String title) {
+public class Main extends MultiStateOverlayExample {
+  public Main(final String title) {
     super(title);
-  }
-
-  @Override
-  public void initStatesList(final GameContainer container) throws SlickException {
-    addState(new TestState1());  
   }
 
   public static void main(final String[] args) {
     try {
-      AppGameContainer container = new AppGameContainer(new OverlayMain("Nifty Overlay over Slick"));
+      AppGameContainer container = new AppGameContainer(new Main("Nifty Overlay over Slick"));
       container.setDisplayMode(1024, 768, false);
       container.setTargetFrameRate(1000);
       container.setMinimumLogicUpdateInterval(1);
