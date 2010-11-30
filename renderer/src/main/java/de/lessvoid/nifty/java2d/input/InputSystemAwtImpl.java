080ed1254501e1a77ed4e7e069c6093aaf8af36a
void256 <void256@users.sourceforge.net>
2010-11-30 22:06:36 +0000

    @public correction of handling mouse clicks

--- renderer/src/main/java/de/lessvoid/nifty/java2d/input/InputSystemAwtImpl.java
+++ renderer/src/main/java/de/lessvoid/nifty/java2d/input/InputSystemAwtImpl.java
@@ -61,14 +61,19 @@ public class InputSystemAwtImpl implements InputSystem, MouseMotionListener,
 	}
 
 	@Override
-	public void mousePressed(MouseEvent mouseEvent) {
-
+  public void mousePressed(MouseEvent mouseEvent) {
+	  // at the moment we only care about the BUTTON1
+	  if (mouseEvent.getButton() == MouseEvent.BUTTON1) {
+	    mouseEvents.add(new MouseInputEvent(mouseEvent.getX(), mouseEvent.getY(), true));
+	  }
 	}
 
 	@Override
 	public void mouseReleased(MouseEvent mouseEvent) {
-		mouseEvents.add(new MouseInputEvent(mouseEvent.getX(), mouseEvent
-				.getY(), mouseEvent.getButton() == MouseEvent.BUTTON1));
+	  // at the moment we only care about the BUTTON1
+	  if (mouseEvent.getButton() == MouseEvent.BUTTON1) {
+	    mouseEvents.add(new MouseInputEvent(mouseEvent.getX(), mouseEvent.getY(), false));
+	  }
 	}
 
 	@Override
