ff630b329f85ccddaafcd4ceb9369a4497508c5d
void256 <void256@users.sourceforge.net>
2010-12-12 17:39:23 +0000

    code format cleanup

--- src/main/java/de/lessvoid/nifty/controls/MenuItemControl.java
+++ src/main/java/de/lessvoid/nifty/controls/MenuItemControl.java
@@ -10,65 +10,63 @@ import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.xml.xpp3.Attributes;
 
 public class MenuItemControl extends AbstractController {
-    private Screen screen;
+  private Screen screen;
+  private Element element;
+  private FocusHandler focusHandler;
 
-    private Element element;
+  public void bind(
+      final Nifty nifty,
+      final Screen screenParam,
+      final Element newElement,
+      final Properties properties,
+      final ControllerEventListener newListener,
+      final Attributes controlDefinitionAttributes) {
+    element = newElement;
+    screen = screenParam;
+  }
 
-    private FocusHandler focusHandler;
+  public void onStartScreen() {
+    focusHandler = screen.getFocusHandler();
+  }
 
-    public void bind(final Nifty nifty, final Screen screenParam, final Element newElement,
-            final Properties properties, final ControllerEventListener newListener,
-            final Attributes controlDefinitionAttributes) {
-        element = newElement;
-        screen = screenParam;
+  public boolean inputEvent(final NiftyInputEvent inputEvent) {
+    if (inputEvent == NiftyInputEvent.NextInputElement) {
+      if (focusHandler != null) {
+        Element nextElement = focusHandler.getNext(element);
+        nextElement.setFocus();
+        return true;
+      }
+    } else if (inputEvent == NiftyInputEvent.PrevInputElement) {
+      if (focusHandler != null) {
+        Element prevElement = focusHandler.getPrev(element);
+        prevElement.setFocus();
+        return true;
+      }
+    } else if (inputEvent == NiftyInputEvent.MoveCursorDown) {
+      if (focusHandler != null) {
+        Element nextElement = focusHandler.getNext(element);
+        if (nextElement.getParent().equals(element.getParent())) {
+          nextElement.setFocus();
+          return true;
+        }
+      }
+    } else if (inputEvent == NiftyInputEvent.MoveCursorUp) {
+      if (focusHandler != null) {
+        Element prevElement = focusHandler.getPrev(element);
+        if (prevElement.getParent().equals(element.getParent())) {
+          prevElement.setFocus();
+          return true;
+        }
+      }
+    } else if (inputEvent == NiftyInputEvent.Activate) {
+      element.onClick();
+      return true;
     }
+    return false;
+  }
 
-    public void onStartScreen() {
-        focusHandler = screen.getFocusHandler();
-    }
-
-    public boolean inputEvent(final NiftyInputEvent inputEvent) {
-        if (inputEvent == NiftyInputEvent.NextInputElement) {
-            if (focusHandler != null) {
-                Element nextElement = focusHandler.getNext(element);
-                nextElement.setFocus();
-                return true;
-            }
-        }
-        else if (inputEvent == NiftyInputEvent.PrevInputElement) {
-            if (focusHandler != null) {
-                Element prevElement = focusHandler.getPrev(element);
-                prevElement.setFocus();
-                return true;
-            }
-        }
-        else if (inputEvent == NiftyInputEvent.MoveCursorDown) {
-            if (focusHandler != null) {
-                Element nextElement = focusHandler.getNext(element);
-                if (nextElement.getParent().equals(element.getParent())) {
-                    nextElement.setFocus();
-                    return true;
-                }
-            }
-        }
-        else if (inputEvent == NiftyInputEvent.MoveCursorUp) {
-            if (focusHandler != null) {
-                Element prevElement = focusHandler.getPrev(element);
-                if (prevElement.getParent().equals(element.getParent())) {
-                    prevElement.setFocus();
-                    return true;
-                }
-            }
-        }
-        else if (inputEvent == NiftyInputEvent.Activate) {
-            element.onClick();
-            return true;
-        }
-        return false;
-    }
-
-    @Override
-    public void onFocus(final boolean getFocus) {
-        super.onFocus(getFocus);
-    }
+  @Override
+  public void onFocus(final boolean getFocus) {
+    super.onFocus(getFocus);
+  }
 }
