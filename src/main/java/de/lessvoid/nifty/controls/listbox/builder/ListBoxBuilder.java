0651287206f796a5ad51188ba28e157123c256b3
void256 <void256@users.sourceforge.net>
2011-01-30 18:44:40 +0000

    @public ListBox tweaks

--- src/main/java/de/lessvoid/nifty/controls/listbox/builder/ListBoxBuilder.java
+++ src/main/java/de/lessvoid/nifty/controls/listbox/builder/ListBoxBuilder.java
@@ -3,11 +3,47 @@ package de.lessvoid.nifty.controls.listbox.builder;
 import de.lessvoid.nifty.builder.ControlBuilder;
 
 public class ListBoxBuilder extends ControlBuilder {
-  public ListBoxBuilder(final String name) {
+  public ListBoxBuilder() {
     super("listBox");
   }
 
-  public ListBoxBuilder(final String name, final String id) {
+  public ListBoxBuilder(final String id) {
     super(id, "listBox");
+  }
+
+  public void displayItems(final int displayItems) {
+    set("displayItems", "4");
+  }
+
+  public void selectionModeSingle() {
+    set("selectionMode", "Single");
+  }
+
+  public void selectionModeMutliple() {
+    set("selectionMode", "Multiple");
+  }
+
+  public void selectionModeDisabled() {
+    set("selectionMode", "Disabled");
+  }
+
+  public void showVerticalScrollbar() {
+    set("vertical", "true");
+  }
+
+  public void showHorizontalScrollbar() {
+    set("horizontal", "true");
+  }
+
+  public void hideVerticalScrollbar() {
+    set("vertical", "false");
+  }
+
+  public void hideHorizontalScrollbar() {
+    set("horizontal", "false");
+  }
+
+  public void viewConverterClass(final Class<?> clazz) {
+    set("viewConverterClass", clazz.getName());
   }
 }
