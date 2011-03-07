95899df2cc94b8be7f9e3f90e06f138996f23d39
void256 <void256@users.sourceforge.net>
2011-03-07 22:57:37 +0000

    @public Effect Hint: two new parameter added: "offsetX" and "offsetY" to change the hint position relative to the element when using an individual style that does not follow the mouse

--- src/main/java/de/lessvoid/nifty/effects/impl/Hint.java
+++ src/main/java/de/lessvoid/nifty/effects/impl/Hint.java
@@ -22,6 +22,8 @@ public class Hint implements EffectImpl {
   private String hintLayerId;
   private String hintPanelId;
   private int hintDelay;
+  private String offsetX;
+  private String offsetY;
 
   public void activate(final Nifty niftyParam, final Element element, final EffectProperties parameter) {
     this.nifty = niftyParam;
@@ -30,6 +32,8 @@ public class Hint implements EffectImpl {
     final String hintStyle = parameter.getProperty("hintStyle", null);
     final String hintText = parameter.getProperty("hintText", "hint: add a 'hintText' attribute to the hint effect :)");
     hintDelay = Integer.valueOf(parameter.getProperty("hintDelay", "0"));
+    offsetX = parameter.getProperty("offsetX", "0");
+    offsetY = parameter.getProperty("offsetY", "0");
 
     // we'll create a new layer on the fly for the hint and because we could really
     // have lots of hints we'll generate a unique id for this new layer
@@ -60,8 +64,8 @@ public class Hint implements EffectImpl {
         if (nifty.getNiftyMouse().getNoMouseMovementTime() > hintDelay) {
           Element hintPanel = hintLayer.findElementByName(hintPanelId);
           if (hintPanel != null) {
-            hintPanel.setConstraintX(new SizeValue(element.getX() + 20 + "px"));
-            hintPanel.setConstraintY(new SizeValue(element.getY() + 20 + "px"));
+            hintPanel.setConstraintX(new SizeValue(getPosX(element, hintPanel, r.getWidth()) + "px"));
+            hintPanel.setConstraintY(new SizeValue(getPosY(element, hintPanel, r.getHeight()) + "px"));
 
             hintLayer.layoutElements();
             hintLayer.show();
@@ -87,4 +91,45 @@ public class Hint implements EffectImpl {
       hintLayer.markForRemoval();
     }
   }
+
+  private int getPosX(final Element element, final Element hintPanel, final int screenWidth) {
+    int pos = 0;
+    if ("center".equals(offsetX)) {
+      pos = element.getX() + element.getWidth() / 2 - hintPanel.getWidth() / 2;
+    } else if ("left".equals(offsetX)) {
+      pos = element.getX();
+    } else if ("right".equals(offsetX)) {
+      pos = element.getX() + element.getWidth() - hintPanel.getWidth();
+    } else {
+      pos = Integer.valueOf(offsetX);
+    }
+    if (pos < 0) {
+      pos = 0;
+    }
+    if (pos + hintPanel.getWidth() > screenWidth) {
+      pos = screenWidth - hintPanel.getWidth();
+    }
+    return pos;
+  }
+
+  private int getPosY(final Element element, final Element hintPanel, final int screenHeight) {
+    int pos = 0;
+    if ("center".equals(offsetY)) {
+      pos = element.getY() + element.getHeight() / 2 - hintPanel.getHeight() / 2;
+    } else if ("top".equals(offsetY)) {
+      pos = element.getY();
+    } else if ("bottom".equals(offsetY)) {
+      pos = element.getY() + element.getHeight() - hintPanel.getHeight();
+    } else {
+      pos = Integer.valueOf(offsetY);
+    }
+    if (pos < 0) {
+      pos = 0;
+    }
+    if (pos + hintPanel.getHeight() > screenHeight) {
+      pos = screenHeight - hintPanel.getHeight();
+    }
+    return pos;
+  }
+
 }
