3e90957333eaa3f1769e068790c282afb1fedafd
void256 <void256@users.sourceforge.net>
2011-04-02 21:58:47 +0000

    @public close popup menu when clicked outside of the actual menu with any mouse button

---
+++ src/main/java/de/lessvoid/nifty/controls/menu/PopupMenuControl.java
@@ -0,0 +1,46 @@
+package de.lessvoid.nifty.controls.menu;
+
+import java.util.Properties;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.Controller;
+import de.lessvoid.nifty.elements.Element;
+import de.lessvoid.nifty.input.NiftyInputEvent;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.xml.xpp3.Attributes;
+
+public class PopupMenuControl<T> implements Controller {
+  private Nifty nifty;
+  private Element element;
+
+  public void bind(
+      final Nifty niftyParam,
+      final Screen screenParam,
+      final Element newElement,
+      final Properties properties,
+      final Attributes controlDefinitionAttributesParam) {
+    nifty = niftyParam;
+    element = newElement;
+  }
+
+  @Override
+  public void init(final Properties parameter, final Attributes controlDefinitionAttributes) {
+  }
+
+  @Override
+  public void onStartScreen() {
+  }
+
+  @Override
+  public boolean inputEvent(final NiftyInputEvent inputEvent) {
+    return false;
+  }
+
+  @Override
+  public void onFocus(final boolean getFocus) {
+  }
+
+  public void closePopup() {
+    nifty.closePopup(element.getId());
+  }
+}
