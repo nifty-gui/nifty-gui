b2f7cfe82c30225222f2c9fb6f0380bf31285a55
void256 <void256@users.sourceforge.net>
2011-05-21 16:06:45 +0000

    example added to show that ${} replacement works when changing elements from java

--- src/main/java/de/lessvoid/nifty/examples/localize/LocalizeTestScreen.java
+++ src/main/java/de/lessvoid/nifty/examples/localize/LocalizeTestScreen.java
@@ -1,14 +1,20 @@
 package de.lessvoid.nifty.examples.localize;
 
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.controls.Label;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.nifty.screen.ScreenController;
 
 public class LocalizeTestScreen implements ScreenController {
+  private Screen screen;
+
   public void bind(final Nifty newNifty, final Screen newScreen) {
+    this.screen = newScreen;
   }
 
   public void onStartScreen() {
+    // #3147118: Apply Special XML Markup replacement ${} on dynamic changes (http://sourceforge.net/tracker/?func=detail&aid=3147118&group_id=223898&atid=1059825)
+    screen.findNiftyControl("label", Label.class).setText("${dialog.hello}");
   }
 
   public void onEndScreen() {
