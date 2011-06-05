92dca0b65550ab43c5b7cb212e3ff8b4f848abe0
void256 <void256@users.sourceforge.net>
2011-06-05 00:13:22 +0000

    @public some more missing methods added to the SliderBuilder

--- src/main/java/de/lessvoid/nifty/controls/slider/builder/SliderBuilder.java
+++ src/main/java/de/lessvoid/nifty/controls/slider/builder/SliderBuilder.java
@@ -11,6 +11,26 @@ public class SliderBuilder extends ControlBuilder {
     super(id, getName(vertical));
   }
 
+  public void min(final float min) {
+    set("min", String.valueOf(min));
+  }
+
+  public void max(final float max) {
+    set("max", String.valueOf(max));
+  }
+
+  public void initial(final float initial) {
+    set("initial", String.valueOf(initial));
+  }
+
+  public void stepSize(final float stepSize) {
+    set("stepSize", String.valueOf(stepSize));
+  }
+
+  public void buttonStepSize(final float buttonStepSize) {
+    set("buttonStepSize", String.valueOf(buttonStepSize));
+  }
+
   private static String getName(final boolean vertical) {
     if (vertical) {
       return "verticalSlider";
