e38d1bf13f4758ae5e0dd664ca6585393ad057a6
void256 <void256@users.sourceforge.net>
2011-06-19 21:46:13 +0000

    fixed junit tests

--- src/test/java/de/lessvoid/nifty/elements/ElementEnableDisableTest.java
+++ src/test/java/de/lessvoid/nifty/elements/ElementEnableDisableTest.java
@@ -1,6 +1,8 @@
 package de.lessvoid.nifty.elements;
 
+import static org.easymock.EasyMock.eq;
 import static org.easymock.EasyMock.expect;
+import static org.easymock.EasyMock.isA;
 import static org.easymock.classextension.EasyMock.createMock;
 import static org.easymock.classextension.EasyMock.replay;
 import static org.easymock.classextension.EasyMock.verify;
@@ -14,6 +16,8 @@ import org.junit.Test;
 
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.controls.FocusHandler;
+import de.lessvoid.nifty.elements.events.ElementDisableEvent;
+import de.lessvoid.nifty.elements.events.ElementEnableEvent;
 import de.lessvoid.nifty.elements.render.ElementRenderer;
 import de.lessvoid.nifty.loaderv2.types.ElementType;
 import de.lessvoid.nifty.tools.TimeProvider;
@@ -28,8 +32,178 @@ public class ElementEnableDisableTest {
   public void before() throws Exception {
     niftyMock = createMock(Nifty.class);
     expect(niftyMock.getAlternateKey()).andReturn(null).times(2);
-    replay(niftyMock);
+  }
 
+  @After
+  public void after() {
+    verify(niftyMock);
+    verify(e1);
+    verify(e2);
+  }
+
+  @Test
+  public void testSimpleDisable() throws Exception {
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    replay(niftyMock);
+    setupMocks();
+
+    e2.disableFocus();
+    replay(e1);
+    replay(e2);
+
+    e2.disable();
+
+    assertTrue(e1.isEnabled());
+    assertFalse(e2.isEnabled());
+  }
+
+  @Test
+  public void testDisableTwice() throws Exception {
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    niftyMock.publishEvent(eq("e1"), isA(ElementEnableEvent.class));
+    replay(niftyMock);
+    setupMocks();
+
+    e2.disableFocus();
+    replay(e1);
+    replay(e2);
+
+    e2.disable();
+    e2.disable();
+
+    assertTrue(e1.isEnabled());
+    assertFalse(e2.isEnabled());
+
+    e2.enable();
+    assertTrue(e2.isEnabled());
+  }
+
+  @Test
+  public void testDisableTwiceAndThenEnable() throws Exception {
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    niftyMock.publishEvent(eq("e1"), isA(ElementEnableEvent.class));
+    replay(niftyMock);
+    setupMocks();
+
+    e2.disableFocus();
+    replay(e1);
+    replay(e2);
+
+    e2.disable();
+    e2.disable();
+    e2.enable();
+
+    assertTrue(e1.isEnabled());
+    assertTrue(e2.isEnabled());
+  }
+
+  @Test
+  public void testSimpleEnable() throws Exception {
+    replay(niftyMock);
+    setupMocks();
+
+    replay(e1);
+    replay(e2);
+
+    e2.enable();
+
+    assertTrue(e1.isEnabled());
+    assertTrue(e2.isEnabled());
+  }
+
+  @Test
+  public void testEnableTwice() throws Exception {
+    replay(niftyMock);
+    setupMocks();
+
+    replay(e1);
+    replay(e2);
+
+    e2.enable();
+    e2.enable();
+
+    assertTrue(e1.isEnabled());
+    assertTrue(e2.isEnabled());
+  }
+
+  @Test
+  public void testEnableTwiceAndThenDisable() throws Exception {
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    replay(niftyMock);
+    setupMocks();
+
+    e2.disableFocus();
+    replay(e1);
+    replay(e2);
+
+    e2.enable();
+    e2.enable();
+    e2.disable();
+
+    assertTrue(e1.isEnabled());
+    assertFalse(e2.isEnabled());
+  }
+
+  @Test
+  public void testParentDisable() throws Exception {
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    replay(niftyMock);
+    setupMocks();
+
+    e2.disableFocus();
+    replay(e1);
+    replay(e2);
+
+    e1.disable();
+
+    assertFalse(e1.isEnabled());
+    assertFalse(e2.isEnabled());
+  }
+
+  @Test
+  public void testParentDisableWithDisabledChild() throws Exception {
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    replay(niftyMock);
+    setupMocks();
+
+    e2.disableFocus();
+    replay(e1);
+    replay(e2);
+
+    e2.disable();
+    e1.disable();
+
+    assertFalse(e1.isEnabled());
+    assertFalse(e2.isEnabled());
+  }
+
+  @Test
+  public void testParentDisableWithDisabledChildAndParentEnable() throws Exception {
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    niftyMock.publishEvent(eq("e1"), isA(ElementDisableEvent.class));
+    niftyMock.publishEvent(eq("e1"), isA(ElementEnableEvent.class));
+    niftyMock.publishEvent(eq("e1"), isA(ElementEnableEvent.class));
+    replay(niftyMock);
+    setupMocks();
+
+    e2.disableFocus();
+    replay(e1);
+    replay(e2);
+
+    e2.disable();
+    e1.disable();
+    e1.enable();
+
+    assertTrue(e1.isEnabled());
+    assertFalse(e2.isEnabled());
+
+    e2.enable();
+    assertTrue(e2.isEnabled());
+  }
+
+  private void setupMocks() throws NoSuchMethodException {
     e1 = createMock(
         Element.class,
         new ConstructorArgs(
@@ -44,133 +218,5 @@ public class ElementEnableDisableTest {
                 niftyMock, null, "e1", null, focusHandler, false, null, null),
                 Element.class.getMethod("disableFocus"));
     e1.add(e2);
-  }
-
-  @After
-  public void after() {
-    verify(niftyMock);
-    verify(e1);
-    verify(e2);
-  }
-
-  @Test
-  public void testSimpleDisable() {
-    e2.disableFocus();
-    replay(e1);
-    replay(e2);
-
-    e2.disable();
-
-    assertTrue(e1.isEnabled());
-    assertFalse(e2.isEnabled());
-  }
-
-  @Test
-  public void testDisableTwice() {
-    e2.disableFocus();
-    replay(e1);
-    replay(e2);
-
-    e2.disable();
-    e2.disable();
-
-    assertTrue(e1.isEnabled());
-    assertFalse(e2.isEnabled());
-
-    e2.enable();
-    assertTrue(e2.isEnabled());
-  }
-
-  @Test
-  public void testDisableTwiceAndThenEnable() {
-    e2.disableFocus();
-    replay(e1);
-    replay(e2);
-
-    e2.disable();
-    e2.disable();
-    e2.enable();
-
-    assertTrue(e1.isEnabled());
-    assertTrue(e2.isEnabled());
-  }
-
-  @Test
-  public void testSimpleEnable() {
-    replay(e1);
-    replay(e2);
-
-    e2.enable();
-
-    assertTrue(e1.isEnabled());
-    assertTrue(e2.isEnabled());
-  }
-
-  @Test
-  public void testEnableTwice() {
-    replay(e1);
-    replay(e2);
-
-    e2.enable();
-    e2.enable();
-
-    assertTrue(e1.isEnabled());
-    assertTrue(e2.isEnabled());
-  }
-
-  @Test
-  public void testEnableTwiceAndThenDisable() {
-    e2.disableFocus();
-    replay(e1);
-    replay(e2);
-
-    e2.enable();
-    e2.enable();
-    e2.disable();
-
-    assertTrue(e1.isEnabled());
-    assertFalse(e2.isEnabled());
-  }
-
-  @Test
-  public void testParentDisable() {
-    e2.disableFocus();
-    replay(e1);
-    replay(e2);
-
-    e1.disable();
-
-    assertFalse(e1.isEnabled());
-    assertFalse(e2.isEnabled());
-  }
-
-  @Test
-  public void testParentDisableWithDisabledChild() {
-    e2.disableFocus();
-    replay(e1);
-    replay(e2);
-
-    e2.disable();
-    e1.disable();
-
-    assertFalse(e1.isEnabled());
-    assertFalse(e2.isEnabled());
-  }
-
-  @Test
-  public void testParentDisableWithDisabledChildAndParentEnable() {
-    e2.disableFocus();
-    replay(e1);
-    replay(e2);
-
-    e2.disable();
-    e1.disable();
-    e1.enable();
-
-    assertTrue(e1.isEnabled());
-    assertFalse(e2.isEnabled());
-
-    e2.enable();
-    assertTrue(e2.isEnabled());
   }
 }
