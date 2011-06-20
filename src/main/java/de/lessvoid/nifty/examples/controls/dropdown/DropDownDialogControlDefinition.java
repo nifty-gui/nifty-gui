347d9f03c3be0018725c594928cec4f5f0f2e216
void256 <void256@users.sourceforge.net>
2011-06-20 00:24:54 +0000

    radiobuttons example added

--- src/main/java/de/lessvoid/nifty/examples/controls/dropdown/DropDownDialogControlDefinition.java
+++ src/main/java/de/lessvoid/nifty/examples/controls/dropdown/DropDownDialogControlDefinition.java
@@ -3,10 +3,14 @@ package de.lessvoid.nifty.examples.controls.dropdown;
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.builder.ControlBuilder;
 import de.lessvoid.nifty.builder.ControlDefinitionBuilder;
+import de.lessvoid.nifty.builder.EffectBuilder;
 import de.lessvoid.nifty.builder.PanelBuilder;
 import de.lessvoid.nifty.controls.button.builder.ButtonBuilder;
+import de.lessvoid.nifty.controls.checkbox.builder.CheckboxBuilder;
 import de.lessvoid.nifty.controls.dropdown.builder.DropDownBuilder;
 import de.lessvoid.nifty.controls.label.builder.LabelBuilder;
+import de.lessvoid.nifty.controls.radiobutton.builder.RadioButtonBuilder;
+import de.lessvoid.nifty.controls.radiobutton.builder.RadioGroupBuilder;
 import de.lessvoid.nifty.examples.controls.common.CommonBuilders;
 import de.lessvoid.nifty.examples.controls.common.DialogPanelControlDefinition;
 
@@ -68,6 +72,143 @@ public class DropDownDialogControlDefinition {
             alignLeft();
             textHAlignLeft();
             width("250px");
+          }});
+        }});
+
+        panel(builders.vspacer());
+        panel(builders.vspacer());
+        panel(builders.vspacer());
+        panel(builders.vspacer());
+        panel(builders.vspacer());
+        panel(new PanelBuilder() {{
+          width("100%");
+          height("1px");
+          backgroundColor("#0008");
+        }});
+        panel(builders.vspacer());
+        control(new RadioGroupBuilder("RadioGroup-1")); // the RadioGroup id is used to link radiobuttons logical together so that only one of them can be active at a certain time
+        panel(new PanelBuilder() {{
+          control(builders.createLabel("Radio Buttons"));
+          childLayoutHorizontal();
+          panel(new PanelBuilder() {{
+            childLayoutVertical();
+            backgroundColor("#8001");
+            paddingLeft("7px");
+            paddingRight("7px");
+            paddingTop("4px");
+            paddingBottom("4px");
+            width("105px");
+            onActiveEffect(new EffectBuilder("border") {{
+              effectParameter("color", "#0008");
+            }});
+            panel(new PanelBuilder() {{
+              childLayoutHorizontal();
+              control(builders.createLabel("Option 1", "60px"));
+              control(new RadioButtonBuilder("option-1") {{
+                group("RadioGroup-1");
+              }});
+            }});
+            panel(new PanelBuilder() {{
+              childLayoutHorizontal();
+              control(builders.createLabel("Option 2", "60px"));
+              control(new RadioButtonBuilder("option-2") {{
+                group("RadioGroup-1");
+              }});
+            }});
+            panel(new PanelBuilder() {{
+              childLayoutHorizontal();
+              control(builders.createLabel("Option 3", "60px"));
+              control(new RadioButtonBuilder("option-3") {{
+                group("RadioGroup-1");
+              }});
+            }});
+            panel(new PanelBuilder() {{
+              childLayoutHorizontal();
+              control(builders.createLabel("Option 4", "60px"));
+              control(new RadioButtonBuilder("option-4") {{
+                group("RadioGroup-1");
+              }});
+            }});
+          }});
+          panel(builders.hspacer("10px"));
+          panel(new PanelBuilder() {{
+            childLayoutVertical();
+            backgroundColor("#8001");
+            paddingLeft("7px");
+            paddingRight("7px");
+            paddingTop("4px");
+            paddingBottom("4px");
+            onActiveEffect(new EffectBuilder("border") {{
+              effectParameter("color", "#0008");
+            }});
+            panel(new PanelBuilder() {{
+              childLayoutHorizontal();
+              control(builders.createLabel("Option 1 Event:"));
+              control(new LabelBuilder("option-1-changed") {{
+                width("*");
+                alignLeft();
+                textVAlignCenter();
+                textHAlignLeft();
+              }});
+            }});
+            panel(new PanelBuilder() {{
+              childLayoutHorizontal();
+              control(builders.createLabel("Option 2 Event:"));
+              control(new LabelBuilder("option-2-changed") {{
+                width("*");
+                alignLeft();
+                textVAlignCenter();
+                textHAlignLeft();
+              }});
+            }});
+            panel(new PanelBuilder() {{
+              childLayoutHorizontal();
+              control(builders.createLabel("Option 3 Event:"));
+              control(new LabelBuilder("option-3-changed") {{
+                width("*");
+                alignLeft();
+                textVAlignCenter();
+                textHAlignLeft();
+              }});
+            }});
+            panel(new PanelBuilder() {{
+              childLayoutHorizontal();
+              control(builders.createLabel("Option 4 Event:"));
+              control(new LabelBuilder("option-4-changed") {{
+                width("*");
+                alignLeft();
+                textVAlignCenter();
+                textHAlignLeft();
+              }});
+            }});
+          }});
+        }});
+        panel(builders.vspacer());
+        panel(builders.vspacer());
+        panel(new PanelBuilder() {{
+          childLayoutHorizontal();
+          control(builders.createLabel("Group Event:"));
+          control(new LabelBuilder("RadioGroup-1-changed") {{
+            width("*");
+            alignLeft();
+            textVAlignCenter();
+            textHAlignLeft();
+          }});
+        }});
+        panel(builders.vspacer());
+        panel(new PanelBuilder() {{
+          childLayoutHorizontal();
+          control(builders.createLabel("Allow Deselect:"));
+          control(new CheckboxBuilder("radioGroupAllowDeselection") {{
+            checked(false);
+          }});
+        }});
+        panel(builders.vspacer());
+        panel(new PanelBuilder() {{
+          childLayoutHorizontal();
+          control(builders.createLabel("Disable:"));
+          control(new CheckboxBuilder("radioGroupDisable") {{
+            checked(false);
           }});
         }});
       }});
--- src/main/java/de/lessvoid/nifty/examples/controls/dropdown/DropDownDialogController.java
+++ src/main/java/de/lessvoid/nifty/examples/controls/dropdown/DropDownDialogController.java
@@ -6,10 +6,14 @@ import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.NiftyEventSubscriber;
 import de.lessvoid.nifty.controls.Button;
 import de.lessvoid.nifty.controls.ButtonClickedEvent;
+import de.lessvoid.nifty.controls.CheckBoxStateChangedEvent;
 import de.lessvoid.nifty.controls.Controller;
 import de.lessvoid.nifty.controls.DropDown;
 import de.lessvoid.nifty.controls.DropDownSelectionChangedEvent;
 import de.lessvoid.nifty.controls.Label;
+import de.lessvoid.nifty.controls.RadioButtonGroup;
+import de.lessvoid.nifty.controls.RadioButtonGroupStateChangedEvent;
+import de.lessvoid.nifty.controls.RadioButtonStateChangedEvent;
 import de.lessvoid.nifty.controls.TextField;
 import de.lessvoid.nifty.controls.TextFieldChangedEvent;
 import de.lessvoid.nifty.elements.Element;
@@ -19,12 +23,14 @@ import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.xml.xpp3.Attributes;
 
 public class DropDownDialogController implements Controller {
+  private Screen screen;
   private TextField addDropDownItemText;
   private Button addDropDownItemButton;
   private DropDown<JustAnExampleModelClass> dropDown;
   private Label selectedItem;
   private Button removeDropDownItemButton;
   private Label selectedIndices;
+  private RadioButtonGroup radioButtonGroup1;
 
   @SuppressWarnings("unchecked")
   @Override
@@ -34,12 +40,14 @@ public class DropDownDialogController implements Controller {
       final Element element,
       final Properties parameter,
       final Attributes controlDefinitionAttributes) {
+    this.screen = screen;
     this.addDropDownItemText = screen.findNiftyControl("addDropDownItemText", TextField.class);
     this.addDropDownItemButton = screen.findNiftyControl("addDropDownItemButton", Button.class);
     this.dropDown = screen.findNiftyControl("dropDown", DropDown.class);
     this.selectedItem = screen.findNiftyControl("selectedItem", Label.class);
     this.removeDropDownItemButton = screen.findNiftyControl("removeDropDownItemButton", Button.class);
     this.selectedIndices = element.findNiftyControl("#selectedIndices", Label.class);
+    this.radioButtonGroup1 = screen.findNiftyControl("RadioGroup-1", RadioButtonGroup.class);
   }
 
   @Override
@@ -100,6 +108,41 @@ public class DropDownDialogController implements Controller {
   @NiftyEventSubscriber(id="removeDropDownItemButton")
   public void onRemoveDropDownItemClicked(final String id, final ButtonClickedEvent event) {
     dropDown.removeItem(dropDown.getSelection());
+  }
+
+  @NiftyEventSubscriber(id="option-1")
+  public void onOption1Changed(final String id, final RadioButtonStateChangedEvent event) {
+    screen.findNiftyControl("option-1-changed", Label.class).setText(String.valueOf(event.isSelected()));
+  }
+
+  @NiftyEventSubscriber(id="option-2")
+  public void onOption2Changed(final String id, final RadioButtonStateChangedEvent event) {
+    screen.findNiftyControl("option-2-changed", Label.class).setText(String.valueOf(event.isSelected()));
+  }
+
+  @NiftyEventSubscriber(id="option-3")
+  public void onOption3Changed(final String id, final RadioButtonStateChangedEvent event) {
+    screen.findNiftyControl("option-3-changed", Label.class).setText(String.valueOf(event.isSelected()));
+  }
+
+  @NiftyEventSubscriber(id="option-4")
+  public void onOption4Changed(final String id, final RadioButtonStateChangedEvent event) {
+    screen.findNiftyControl("option-4-changed", Label.class).setText(String.valueOf(event.isSelected()));
+  }
+
+  @NiftyEventSubscriber(id="radioGroupAllowDeselection")
+  public void onRadioGroupAllowDeselectionChanged(final String id, final CheckBoxStateChangedEvent event) {
+    radioButtonGroup1.allowDeselection(event.isChecked());
+  }
+
+  @NiftyEventSubscriber(id="RadioGroup-1")
+  public void onRadioGroup1Changed(final String id, final RadioButtonGroupStateChangedEvent event) {
+    screen.findNiftyControl("RadioGroup-1-changed", Label.class).setText(event.getSelectedId() + " (" + event.getPreviousSelectedId() + ")");
+  }
+
+  @NiftyEventSubscriber(id="radioGroupDisable")
+  public void onRadioGroupDisableCheckBox(final String id, final CheckBoxStateChangedEvent event) {
+    radioButtonGroup1.setEnabled(!event.isChecked());
   }
 
   private void setDropDownItemButtonState() {
