8798c97309a49e4929d4b8e7f1f62249bdc4e8d7
void <void256@users.sourceforge.net>
2011-12-28 00:45:46 +0100

    removed unused ParameterResolver class

--- nifty-core/src/main/java/de/lessvoid/nifty/loaderv2/types/resolver/parameter/ParameterResolverDefault.java
+++
@@ -1,9 +0,0 @@
-package de.lessvoid.nifty.loaderv2.types.resolver.parameter;
-
-import de.lessvoid.xml.xpp3.Attributes;
-
-public class ParameterResolverDefault implements ParameterResolver {
-  public Attributes resolve(final Attributes attributes) {
-    return attributes;
-  }
-}
