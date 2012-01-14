a29b34a50f7f404d06541d4976e1accdf6f52931
Martin Karing <nitram@illarion.org>
2012-01-14 11:56:42 +0100

    Fixing Bug of the color validator and improved its performance - isValid function only worked in case the color text fitted the short or the long mode, not in case the without alpha variants were used - Improved usage of the regular expressions to detect valid strings - Changed separation of the different color modes to use the length of the string instead of individual patterns

--- nifty-core/src/main/java/de/lessvoid/nifty/tools/ColorValidator.java
+++ nifty-core/src/main/java/de/lessvoid/nifty/tools/ColorValidator.java
@@ -1,37 +1,99 @@
 package de.lessvoid.nifty.tools;
 
+import java.util.regex.Pattern;
+
 /**
  * This checks a given String that represents a color for being valid. Supported
  * are both short mode "#f12f" and long mode "#ff1122ff"
+ * 
  * @author void
+ * @author Martin Karing &lt;nitram@illarion.org&gt;
  */
 public class ColorValidator {
+  /**
+   * The pattern used to check if the string is valid to be a color definition.
+   */
+  private static final Pattern colorPattern = Pattern.compile("#\\p{XDigit}{3,8}");
+
+  /**
+   * Check if a string fits any type of color definition string.
+   * 
+   * @param toCheck
+   *          the text to check
+   * @return <code>true</code> in case the text is a color definition
+   */
   public boolean isValid(final String toCheck) {
     if (toCheck == null) {
       return false;
     }
-    if (isShortMode(toCheck)) {
-      return true;
-    }
-    if (isLongMode(toCheck)) {
-      return true;
-    }
-    return false;
+
+    return colorPattern.matcher(toCheck).matches();
   }
 
+  /**
+   * Check if the color text is written in the short form with alpha. The text
+   * would have to look like this: <code>#rgba</code>
+   * 
+   * @param toCheck
+   *          the text to check
+   * @return <code>true</code> in case the text is a short form color definition
+   *         with alpha
+   */
   public boolean isShortMode(final String toCheck) {
-    return toCheck.matches("#[0-9a-fA-F]{4}");
-  }  
-
-  public boolean isShortModeWithoutAlpha(final String toCheck) {
-    return toCheck.matches("#[0-9a-fA-F]{3}");
-  }  
-
-  public boolean isLongMode(final String toCheck) {
-    return toCheck.matches("#[0-9a-fA-F]{8}");
+    return isColor(toCheck, 4);
   }
 
+  /**
+   * Check if the color text is written in the short form without alpha. The
+   * text would have to look like this: <code>#rgb</code>
+   * 
+   * @param toCheck
+   *          the text to check
+   * @return <code>true</code> in case the text is a short form color definition
+   *         without alpha
+   */
+  public boolean isShortModeWithoutAlpha(final String toCheck) {
+    return isColor(toCheck, 3);
+  }
+
+  /**
+   * Check if the color text is written in the long form with alpha. The text
+   * would have to look like this: <code>#rrggbbaa</code>
+   * 
+   * @param toCheck
+   *          the text to check
+   * @return <code>true</code> in case the text is a long form color definition
+   *         with alpha
+   */
+  public boolean isLongMode(final String toCheck) {
+    return isColor(toCheck, 8);
+  }
+
+  /**
+   * Check if the color text is written in the long form without alpha. The text
+   * would have to look like this: <code>#rrggbb</code>
+   * 
+   * @param toCheck
+   *          the text to check
+   * @return <code>true</code> in case the text is a long form color definition
+   *         without alpha
+   */
   public boolean isLongModeWithoutAlpha(final String toCheck) {
-    return toCheck.matches("#[0-9a-fA-F]{6}");
+    return isColor(toCheck, 6);
+  }
+
+  /**
+   * Test of a text contains a color definition with a specified amount of color
+   * digits.
+   * 
+   * @param toCheck
+   *          the text to check
+   * @param components
+   *          the amount of digits to be used to define the color
+   * @return <code>true</code> if the text fits a color text and meets the
+   *         required length exactly
+   */
+  private boolean isColor(final String toCheck, final int components) {
+    return (toCheck.length() == (components + 1) && isValid(toCheck));
   }
 }
