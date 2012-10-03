74b176d3adff64d749277a1e8f4a8400226bbc68
void <void256@users.sourceforge.net>
2012-10-04 00:55:36 +0200

    correctly close Inputstream when reading the jdk14 log configuration

--- nifty-examples/src/main/java/de/lessvoid/nifty/examples/LoggerShortFormat.java
+++ nifty-examples/src/main/java/de/lessvoid/nifty/examples/LoggerShortFormat.java
@@ -1,23 +1,26 @@
 package de.lessvoid.nifty.examples;
 
+import java.io.InputStream;
 import java.util.logging.LogManager;
 import java.util.logging.LogRecord;
 
 public class LoggerShortFormat extends java.util.logging.Formatter {
   public String format(final LogRecord record) {
-     return
-       record.getMillis() + " " +
-       record.getLevel() + " [" +
-       record.getSourceClassName() + "] " +
-       record.getMessage() + "\n";
+    return record.getMillis() + " " +
+        record.getLevel() + " [" +
+        record.getSourceClassName() + "] " +
+        record.getMessage() + "\n";
   }
-  
-  public static void intialize() {
+
+  public static void intialize() throws Exception {
+    InputStream input = null;
     try {
-      LogManager.getLogManager().readConfiguration(LoggerShortFormat.class.getClassLoader().getResourceAsStream("logging.properties"));
-    } catch (Exception e) {
-      e.printStackTrace();
-      throw new RuntimeException(e);
+      input = LoggerShortFormat.class.getClassLoader().getResourceAsStream("logging.properties");
+      LogManager.getLogManager().readConfiguration(input);
+    } finally {
+      if (input != null) {
+        input.close();
+      }
     }
   }
 }
