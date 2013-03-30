4134cb5f02a79e07bfeacfd31753a9e0bf7d9709
void <void256@users.sourceforge.net>
2013-03-30 20:01:21 +0100

    renamed example runner to RunUseCaseLwjglMain and allow class on cmdline

--- nifty-core-examples/src/main/java/de/lessvoid/nifty/examples/usecase/RunUseCaseLwjglMain.java
+++ nifty-core-examples/src/main/java/de/lessvoid/nifty/examples/usecase/RunUseCaseLwjglMain.java
@@ -1,17 +1,22 @@
-package de.lessvoid.nifty.examples;
+package de.lessvoid.nifty.examples.usecase;
 
 import java.util.logging.Logger;
 
 import de.lessvoid.coregl.CoreLwjglSetup;
 import de.lessvoid.coregl.CoreLwjglSetup.RenderLoopCallback;
 import de.lessvoid.nifty.api.Nifty;
-import de.lessvoid.nifty.examples.usecase.UseCase_0001_FullScreenColorNode;
-import de.lessvoid.nifty.examples.usecase.UseCase_0002_QuarterRootNodeWithTwoHorizontalChildNodes;
 import de.lessvoid.nifty.renderer.lwjgl.NiftyRenderDeviceLwgl;
 
-public class NiftyExampleLwjglMain {
+public class RunUseCaseLwjglMain {
+  private static Logger log = Logger.getLogger(RunUseCaseLwjglMain.class.getName());
 
   public static void main(final String[] args) throws Exception {
+    if (args.length != 1) {
+      System.err.println("Expected use case classname (without package name) to run.");
+      System.err.println("Example: " + RunUseCaseLwjglMain.class.getName() + " UseCase_0001_FullScreenColorNode");
+      System.exit(1);
+    }
+
     // init LWJGL using some helper class
     CoreLwjglSetup setup = new CoreLwjglSetup();
     setup.initializeLogging();
@@ -20,10 +25,7 @@ public class NiftyExampleLwjglMain {
     // create nifty instance
     final Nifty nifty = createNifty();
 
-    // FIXME allow example class to be given by commandline, so that we can use this code to run multiple examples 
-    new UseCase_0001_FullScreenColorNode(nifty);
-    new UseCase_0002_QuarterRootNodeWithTwoHorizontalChildNodes(nifty);
-
+    loadUseCase(args[0], nifty);
     logScene(nifty);
 
     setup.renderLoop(new RenderLoopCallback() {
@@ -36,8 +38,15 @@ public class NiftyExampleLwjglMain {
     });
   }
 
+  private static void loadUseCase(final String clazzName, final Nifty nifty) throws Exception {
+    Class<?> clazz = ClassLoader.getSystemClassLoader().loadClass(
+        RunUseCaseLwjglMain.class.getPackage().getName() + "." + clazzName);
+    log.info("loaded class [" + clazz + "]");
+
+    clazz.getConstructor(Nifty.class).newInstance(nifty);
+  }
+
   private static void logScene(final Nifty nifty) {
-    Logger log = Logger.getLogger(NiftyExampleLwjglMain.class.getName());
     log.info(nifty.getSceneInfoLog());
   }
 
