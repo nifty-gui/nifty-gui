34e2a577e6f2bd2bb4a8a8933e87dd0a8e6e6070
void <void256@users.sourceforge.net>
2013-04-30 01:03:13 +0200

    JOGL examples project added

---
+++ nifty-examples-jogl/src/main/java/de/lessvoid/nifty/examples/jogl/ResolutionControlJOGL.java
@@ -0,0 +1,22 @@
+package de.lessvoid.nifty.examples.jogl;
+
+import java.util.Collection;
+
+import de.lessvoid.nifty.examples.resolution.ResolutionControl;
+
+public class ResolutionControlJOGL implements ResolutionControl<Object> {
+
+  @Override
+  public Collection<Object> getResolutions() {
+    return null;
+  }
+
+  @Override
+  public void setResolution(Object newResolution) {
+  }
+
+  @Override
+  public Object getCurrentResolution() {
+    return null;
+  }
+}
