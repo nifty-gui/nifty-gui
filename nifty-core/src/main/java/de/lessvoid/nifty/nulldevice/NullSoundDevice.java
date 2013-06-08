aed1455252c672849b8b561b55fe58aa043c21f1
void <void256@users.sourceforge.net>
2013-06-08 22:44:50 +0200

    added NullSoundHandle to NullSoundDevice for improved stability

--- nifty-core/src/main/java/de/lessvoid/nifty/nulldevice/NullSoundDevice.java
+++ nifty-core/src/main/java/de/lessvoid/nifty/nulldevice/NullSoundDevice.java
@@ -12,15 +12,44 @@ public class NullSoundDevice implements SoundDevice {
   
   @Override
   public SoundHandle loadMusic(SoundSystem soundSystem, String filename) {
-    return null;
+    return new NullSoundHandle();
   }
   
   @Override
   public SoundHandle loadSound(SoundSystem soundSystem, String filename) {
-    return null;
+    return new NullSoundHandle();
   }
   
   @Override
   public void update(int delta) {
+  }
+
+  public static class NullSoundHandle implements SoundHandle {
+
+    @Override
+    public void play() {
+    }
+
+    @Override
+    public void stop() {
+    }
+
+    @Override
+    public void setVolume(float volume) {
+    }
+
+    @Override
+    public float getVolume() {
+      return 0;
+    }
+
+    @Override
+    public boolean isPlaying() {
+      return false;
+    }
+
+    @Override
+    public void dispose() {
+    }
   }
 }
