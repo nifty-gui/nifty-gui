0ad6c94045dc5a107fd98b4b51691cbe460f4b86
void <void256@users.sourceforge.net>
2013-06-09 00:19:02 +0200

    Enabled Core-Profile/Batch-Rendering for TutorialMainNativeLWJGL

--- nifty-examples/src/main/java/de/lessvoid/nifty/examples/tutorial/TutorialMainNativeLwjgl.java
+++ nifty-examples/src/main/java/de/lessvoid/nifty/examples/tutorial/TutorialMainNativeLwjgl.java
@@ -6,6 +6,7 @@ import de.lessvoid.nifty.examples.LwjglInitHelper;
 import de.lessvoid.nifty.renderer.lwjgl.input.LwjglInputSystem;
 import de.lessvoid.nifty.renderer.lwjgl.render.LwjglRenderDevice;
 import de.lessvoid.nifty.renderer.lwjgl.render.batch.LwjglBatchRenderBackend;
+import de.lessvoid.nifty.renderer.lwjgl.render.batch.LwjglBatchRenderBackendCoreProfile;
 import de.lessvoid.nifty.sound.openal.OpenALSoundDevice;
 import de.lessvoid.nifty.spi.time.impl.AccurateTimeProvider;
 
@@ -15,15 +16,15 @@ public final class TutorialMainNativeLwjgl {
   }
 
   public static void main(final String[] args) throws Exception {
-    if (!LwjglInitHelper.initSubSystems("Nifty 1.2 Tutorial, rendering using native Lwjgl with OpenAL Sound")) {
+    if (!LwjglInitHelper.initSubSystems("Nifty 1.2 Tutorial, rendering using native Lwjgl with OpenAL Sound", true)) {
       System.exit(0);
     }
 
     // create nifty
-    BatchRenderDevice renderDevice = new BatchRenderDevice(new LwjglBatchRenderBackend(), 2048, 2048);
+    BatchRenderDevice renderDevice = new BatchRenderDevice(new LwjglBatchRenderBackendCoreProfile(), 2048, 2048);
     renderDevice.enableLogFPS();
     Nifty nifty = new Nifty(
-        new LwjglRenderDevice(),
+        renderDevice,
         new OpenALSoundDevice(),
         new LwjglInputSystem(),
         new AccurateTimeProvider());
