1513a2e8f77ec420ac8febda30a68555279cba0e
void <void256@users.sourceforge.net>
2013-09-22 14:24:34 +0200

    #139 fix display of initial CheckBox state
    The CheckBox didn't display its correct state when it was shown after
    being hidden.
    
    Example: CheckBox on a hidden Tab and change it's state by code. When
    you then display the tab the Checkbox won't show it's correct state.
    
    I've modified the TabsControlDialog in the standard controls demo to
    test this.

--- nifty-examples/src/main/java/de/lessvoid/nifty/examples/defaultcontrols/tabs/TabsControlDialogController.java
+++ nifty-examples/src/main/java/de/lessvoid/nifty/examples/defaultcontrols/tabs/TabsControlDialogController.java
@@ -3,12 +3,17 @@ package de.lessvoid.nifty.examples.defaultcontrols.tabs;
 import java.util.Properties;
 
 import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.NiftyEventSubscriber;
+import de.lessvoid.nifty.controls.CheckBox;
+import de.lessvoid.nifty.controls.CheckBoxStateChangedEvent;
 import de.lessvoid.nifty.controls.Controller;
 import de.lessvoid.nifty.controls.TabGroup;
+import de.lessvoid.nifty.controls.checkbox.builder.CheckboxBuilder;
 import de.lessvoid.nifty.controls.label.builder.LabelBuilder;
 import de.lessvoid.nifty.controls.tabs.builder.TabBuilder;
 import de.lessvoid.nifty.elements.Element;
-import de.lessvoid.nifty.examples.defaultcontrols.common.CommonBuilders;
+import de.lessvoid.nifty.elements.events.NiftyMousePrimaryClickedEvent;
+import de.lessvoid.nifty.elements.render.TextRenderer;
 import de.lessvoid.nifty.input.NiftyInputEvent;
 import de.lessvoid.nifty.screen.Screen;
 import de.lessvoid.xml.xpp3.Attributes;
@@ -22,7 +27,7 @@ import de.lessvoid.xml.xpp3.Attributes;
 public class TabsControlDialogController implements Controller {
 
   private TabGroup tabs;
-  private static CommonBuilders builders = new CommonBuilders();
+  private Screen screen;
 
   @Override
   public void bind(
@@ -31,21 +36,25 @@ public class TabsControlDialogController implements Controller {
       final Element element,
       final Properties parameter,
       final Attributes controlDefinitionAttributes) {
+    this.screen = screen;
     tabs = screen.findNiftyControl("tabs", TabGroup.class);
     tabs.addTab(new TabBuilder("tab_1", "Tab 1") {{
-      childLayoutCenter();
-      control(new LabelBuilder("tab1_label", "Tab 1") {{
-        height("100%");
-        width("100%");
-      }});
+      childLayoutVertical();
       height("100%");
       width("100%");
+      control(new LabelBuilder("tab1_label", "Tab 1") {{
+        height("50%");
+        width("100%");
+      }});
+      control(new LabelBuilder("tab1_label_checkboxstate", "Checkbox state: ---") {{
+        height("50%");
+        width("100%");
+        visibleToMouse();
+      }});
     }});
     tabs.addTab(new TabBuilder("tab_2", "Tab 2") {{
       childLayoutCenter();
-      control(new LabelBuilder("tab2_label", "Tab 2") {{
-        height("100%");
-        width("100%");
+      control(new CheckboxBuilder("tab_2_checkbox") {{
       }});
       height("100%");
       width("100%");
@@ -58,6 +67,7 @@ public class TabsControlDialogController implements Controller {
 
   @Override
   public void onStartScreen() {
+    displayCheckBoxState(screen.findNiftyControl("tab_2_checkbox", CheckBox.class));
   }
 
   @Override
@@ -67,5 +77,23 @@ public class TabsControlDialogController implements Controller {
   @Override
   public boolean inputEvent(final NiftyInputEvent inputEvent) {
     return false;
+  }
+
+  @NiftyEventSubscriber(id="tab1_label_checkboxstate")
+  public void onLabelClick(final String id, final NiftyMousePrimaryClickedEvent label) {
+    CheckBox checkBox = screen.findNiftyControl("tab_2_checkbox", CheckBox.class);
+    checkBox.toggle();
+    displayCheckBoxState(checkBox);
+  }
+
+  @NiftyEventSubscriber(id="tab_2_checkbox")
+  public void checkBoxStateChange(final String id, final CheckBoxStateChangedEvent stateChanged) {
+    displayCheckBoxState(stateChanged.getCheckBox());
+  }
+
+  private void displayCheckBoxState(final CheckBox checkBox) {
+    screen.findElementByName("tab1_label_checkboxstate")
+      .getRenderer(TextRenderer.class)
+      .setText("Checkbox state (on Tab 2)\nisChecked(): " + checkBox.isChecked() + "\n\n(click me!)");
   }
 }
