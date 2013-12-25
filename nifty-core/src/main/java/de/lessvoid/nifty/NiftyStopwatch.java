a0bb072df45a8d5b4f79cd574bc2fa17ff1f85e3
Martin Karing <nitram@illarion.org>
2013-12-25 18:12:05 +0100

    Cleanup of NiftyStopWatch
     - Slightly improved performance by using a not synchronized collection
     - Avoided creation of log message in case logging is disabled
     - Avoided object creation to receive timestamp

--- nifty-core/src/main/java/de/lessvoid/nifty/NiftyStopwatch.java
+++ nifty-core/src/main/java/de/lessvoid/nifty/NiftyStopwatch.java
@@ -1,33 +1,83 @@
 package de.lessvoid.nifty;
 
 import javax.annotation.Nonnull;
-import java.util.Date;
-import java.util.Stack;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.logging.Level;
 import java.util.logging.Logger;
 
+/**
+ * This stop watch is used to measure the time required for special operations. This is only used for logging purposes.
+ *
+ * @author void
+ * @author Martin Karing &lt;nitram@illarion.org&gt;
+ */
 public class NiftyStopwatch {
-  private static final Logger log = Logger.getLogger(NiftyStopwatch.class.getName());
+  /**
+   * The logger that handles showing the output of this class.
+   */
   @Nonnull
-  private static final Stack<Long> stack = new Stack<Long>();
+  private static final Logger log = Logger.getLogger(NiftyStopwatch.class.getName());
 
+  /**
+   * The level used to show the logging output.
+   */
+  @Nonnull
+  private static final Level usedLogLevel = Level.FINE;
+
+  /**
+   * The LIFO queue used to store the time values
+   */
+  @Nonnull
+  private static final Queue<Long> stack = Collections.asLifoQueue(new LinkedList<Long>());
+
+  /**
+   * Private constructor
+   */
+  private NiftyStopwatch() {
+  }
+
+  /**
+   * Start measuring the time.
+   */
   public static void start() {
-    stack.push(now());
+    stack.offer(now());
   }
 
-  public static void stop(final String message) {
-    long length = now() - stack.pop();
-    StringBuilder b = new StringBuilder();
-    for (int i = 0; i < stack.size(); i++) {
-      b.append(".");
+  /**
+   * Stop the time measurement and show the logging output along with the supplied message.
+   *
+   * @param message the message to show in the logging output
+   */
+  public static void stop(@Nonnull final String message) {
+    long length = stop();
+    if (log.isLoggable(usedLogLevel)) {
+      StringBuilder b = new StringBuilder();
+      b.append("[").append(String.format("%04d", length)).append("] ");
+      for (int i = 0; i < stack.size(); i++) {
+        b.append(".");
+      }
+      b.append(' ').append(message);
+      log.log(usedLogLevel, b.toString());
     }
-    log.fine("[" + String.format("%04d", length) + "] " + b + message);
   }
 
+  /**
+   * Stop the time measuring.
+   *
+   * @return the time since the last start call in milliseconds
+   */
   public static long stop() {
-    return now() - stack.pop();
+    return now() - stack.poll();
   }
 
+  /**
+   * Get the current timestamp.
+   *
+   * @return the current timestamp
+   */
   private static long now() {
-    return new Date().getTime();
+    return System.currentTimeMillis();
   }
 }
