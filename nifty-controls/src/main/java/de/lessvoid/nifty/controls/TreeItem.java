e3487c1ef56169d64aae40b2e4901b071b62e427
Torge Rothe <toggie@gmx.de>
2014-01-25 01:55:41 +0100

    Fix treetime format

--- nifty-controls/src/main/java/de/lessvoid/nifty/controls/TreeItem.java
+++ nifty-controls/src/main/java/de/lessvoid/nifty/controls/TreeItem.java
@@ -4,13 +4,12 @@
  */
 package de.lessvoid.nifty.controls;
 
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
-
-import javax.annotation.Nonnull;
-import javax.annotation.Nullable;
 
 /**
  * This class represents any element in a tree list.
@@ -18,120 +17,108 @@ import javax.annotation.Nullable;
  * @param <T> the type that is displayed in the node
  */
 public final class TreeItem<T> implements Iterable<TreeItem<T>> {
-    /**
-     * The children of this tree item.
-     */
-    @Nonnull
-    private final List<TreeItem<T>> children;
+  /**
+   * The children of this tree item.
+   */
+  @Nonnull
+  private final List<TreeItem<T>> children;
 
-    /**
-     * The parent of this item.
-     */
-    @Nullable
-    private TreeItem<T> parentItem;
+  /**
+   * The parent of this item.
+   */
+  @Nullable
+  private TreeItem<T> parentItem;
 
-    /**
-     * The current state of the item. This only applies in case the node is not the root node and not a leaf.
-     */
-    private boolean expanded;
+  /**
+   * The current state of the item. This only applies in case the node is not the root node and not a leaf.
+   */
+  private boolean expanded;
 
-    /**
-     * The actual value inside this tree node.
-     */
-    @Nullable
-    private T value;
+  /**
+   * The actual value inside this tree node.
+   */
+  @Nullable
+  private T value;
 
-    /**
-     * The value how much the tree item is indented. This value is altered by the tree box control.
-     */
-    private int indent;
+  /**
+   * The value how much the tree item is indented. This value is altered by the tree box control.
+   */
+  private int indent;
 
-    /**
-     * Default item for a constructor.
-     */
-    public TreeItem() {
-        children = new ArrayList<TreeItem<T>>();
-    }
+  /**
+   * Default item for a constructor.
+   */
+  public TreeItem() {
+    children = new ArrayList<TreeItem<T>>();
+  }
 
-    /**
-     * Default item for a constructor.
-     */
-    public TreeItem(@Nullable final T itemValue) {
-        this();
-        value = itemValue;
-    }
+  /**
+   * Default item for a constructor.
+   */
+  public TreeItem(@Nullable final T itemValue) {
+    this();
+    value = itemValue;
+  }
 
-    public void addTreeItem(final TreeItem<T> item) {
-        children.add(item);
-        item.setParentItem(this);
-    }
+  public void addTreeItem(final TreeItem<T> item) {
+    children.add(item);
+  }
 
-    public void addTreeItems(@Nonnull final Collection<TreeItem<T>> items) {
-        for (TreeItem<T> t : items)
-            addTreeItem(t);
-    }
+  public void addTreeItems(@Nonnull final Collection<TreeItem<T>> items) {
+    children.addAll(items);
+  }
 
-    public void removeTreeItem(final TreeItem<T> item) {
-        children.remove(item);
-        item.setParentItem(null);
-    }
+  /**
+   * Returns an iterator over a set of elements of type T.
+   *
+   * @return an Iterator.
+   */
+  @Nonnull
+  @Override
+  public Iterator<TreeItem<T>> iterator() {
+    return children.iterator();
+  }
 
-    public void removeTreeItems(@Nonnull final Collection<TreeItem<T>> items) {
-        for (TreeItem<T> t : items)
-            removeTreeItem(t);
-    }
+  @Nullable
+  public T getValue() {
+    return value;
+  }
 
-    /**
-     * Returns an iterator over a set of elements of type T.
-     *
-     * @return an Iterator.
-     */
-    @Nonnull
-    @Override
-    public Iterator<TreeItem<T>> iterator() {
-        return children.iterator();
-    }
+  public void setValue(@Nullable final T value) {
+    this.value = value;
+  }
 
-    @Nullable
-    public T getValue() {
-        return value;
-    }
+  @Nullable
+  public TreeItem<T> getParentItem() {
+    return parentItem;
+  }
 
-    public void setValue(@Nullable final T value) {
-        this.value = value;
-    }
+  public void setParentItem(@Nullable final TreeItem<T> parentItem) {
+    this.parentItem = parentItem;
+  }
 
-    @Nullable
-    public TreeItem<T> getParentItem() {
-        return parentItem;
-    }
+  public boolean isExpanded() {
+    return expanded;
+  }
 
-    public void setParentItem(@Nullable final TreeItem<T> parentItem) {
-        this.parentItem = parentItem;
-    }
+  public void setExpanded(final boolean expanded) {
+    this.expanded = expanded;
+  }
 
-    public boolean isExpanded() {
-        return expanded;
-    }
+  /**
+   * Check if this tree item is a leaf. So if it does not have any children.
+   *
+   * @return {@code true} in case this tree item is a leaf
+   */
+  public boolean isLeaf() {
+    return children.isEmpty();
+  }
 
-    public void setExpanded(final boolean expanded) {
-        this.expanded = expanded;
-    }
+  public int getIndent() {
+    return indent;
+  }
 
-    /**
-     * Check if this tree item is a leaf. So if it does not have any children.
-     *
-     * @return {@code true} in case this tree item is a leaf
-     */
-    public boolean isLeaf() {
-        return children.isEmpty();
-    }
-
-    public int getIndent() {
-        return indent;
-    }
-
-    public void setIndent(final int indent) {
-        this.indent = indent;
-    }
+  public void setIndent(final int indent) {
+    this.indent = indent;
+  }
 }
