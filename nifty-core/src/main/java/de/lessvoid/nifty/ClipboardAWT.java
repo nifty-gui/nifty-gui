a57589e832f348a990131a0ce46dbc7137e94de4
Martin Karing <nitram@illarion.org>
2014-08-06 22:33:16 +0200

    Catching a possible NPE on linux systems that are caused by a bug in the java runtime.
    For further information: JDK-6322854

--- nifty-core/src/main/java/de/lessvoid/nifty/ClipboardAWT.java
+++ nifty-core/src/main/java/de/lessvoid/nifty/ClipboardAWT.java
@@ -1,6 +1,5 @@
 package de.lessvoid.nifty;
 
-import javax.annotation.Nullable;
 import java.awt.*;
 import java.awt.datatransfer.DataFlavor;
 import java.awt.datatransfer.StringSelection;
@@ -9,6 +8,7 @@ import java.awt.datatransfer.UnsupportedFlavorException;
 import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.Nullable;
 
 /**
  * AWT based clipboard implementation.
@@ -44,16 +44,21 @@ public class ClipboardAWT implements Clipboard {
     java.awt.datatransfer.Clipboard clipboard = Toolkit.getDefaultToolkit().getSystemClipboard();
 
     //odd: the Object param of getContents is not currently used
-    Transferable contents = clipboard.getContents(null);
-    boolean hasTransferableText = (contents != null) && contents.isDataFlavorSupported(DataFlavor.stringFlavor);
-    if (hasTransferableText) {
-      try {
-        return contents.getTransferData(DataFlavor.stringFlavor).toString();
-      } catch (UnsupportedFlavorException ex) {
-        log.log(Level.WARNING, "DataFlavor not supported!", ex);
-      } catch (IOException ex) {
-        log.log(Level.INFO, "Conversation", ex);
+    try {
+      Transferable contents = clipboard.getContents(null);
+      boolean hasTransferableText = (contents != null) && contents.isDataFlavorSupported(DataFlavor.stringFlavor);
+      if (hasTransferableText) {
+        try {
+          return contents.getTransferData(DataFlavor.stringFlavor).toString();
+        } catch (UnsupportedFlavorException ex) {
+          log.log(Level.WARNING, "DataFlavor not supported!", ex);
+        } catch (IOException ex) {
+          log.log(Level.INFO, "Conversation", ex);
+        }
       }
+    } catch (NullPointerException ex) {
+      // This NullPointerException due to JDK-6322854 on linux systems
+      log.log(Level.WARNING, "Internal error in retrieving the clipboard data.", ex);
     }
     return null;
   }
