7e42cbafca033e9df8a2b8c853d439ebca68a279
void <void@lessvoid.com>
2014-10-31 01:33:12 +0100

    chore: fix failing unit tests

--- nifty/src/test/java/de/lessvoid/nifty/internal/InternalNiftyNodeInputHandlerTest.java
+++ nifty/src/test/java/de/lessvoid/nifty/internal/InternalNiftyNodeInputHandlerTest.java
@@ -10,26 +10,32 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import de.lessvoid.nifty.api.NiftyNode;
+import de.lessvoid.nifty.api.event.NiftyMouseEnterNodeEvent;
 import de.lessvoid.nifty.api.event.NiftyMouseHoverEvent;
 import de.lessvoid.nifty.api.input.NiftyPointerEvent;
 import de.lessvoid.nifty.internal.math.Vec4;
 
 public class InternalNiftyNodeInputHandlerTest {
   private InternalNiftyNodeInputHandler inputHandler;
-  private InternalNiftyNode niftyNode;
+  private InternalNiftyNode internalNiftyNode;
   private InternalNiftyEventBus eventBus;
+  private NiftyNode niftyNode;
 
   @Before
   public void before() {
-    niftyNode = createMock(InternalNiftyNode.class);
+    internalNiftyNode = createMock(InternalNiftyNode.class);
     eventBus = createMock(InternalNiftyEventBus.class);
-    inputHandler = new InternalNiftyNodeInputHandler(niftyNode);
+    inputHandler = new InternalNiftyNodeInputHandler(internalNiftyNode);
+    niftyNode = createMock(NiftyNode.class);
+    replay(niftyNode);
   }
 
   @After
   public void after() {
-    verify(niftyNode);
+    verify(internalNiftyNode);
     verify(eventBus);
+    verify(niftyNode);
   }
 
   @Test
@@ -70,25 +76,26 @@ public class InternalNiftyNodeInputHandlerTest {
   @Test
   public void testOutside() {
     replay(eventBus);
-    expect(niftyNode.getWidth()).andReturn(100);
-    expect(niftyNode.screenToLocal(150, 150)).andReturn(new Vec4(150.f, 150.f, 0.f, 1.f));
-    replay(niftyNode);
+    expect(internalNiftyNode.getWidth()).andReturn(100);
+    expect(internalNiftyNode.screenToLocal(150, 150)).andReturn(new Vec4(150.f, 150.f, 0.f, 1.f));
+    replay(internalNiftyNode);
 
     inputHandler.pointerEvent(eventBus, makePointerEvent(150, 150));
   }
 
   private void prepareEventBusMock() {
+    eventBus.publish(isA(NiftyMouseEnterNodeEvent.class));
     eventBus.publish(isA(NiftyMouseHoverEvent.class));
     replay(eventBus);
   }
 
   private void prepareMock(final int x, final int y) {
-    expect(niftyNode.getNiftyNode()).andReturn(null);
-    expect(niftyNode.getWidth()).andReturn(100);
-    expect(niftyNode.getHeight()).andReturn(100);
-    expect(niftyNode.screenToLocal(x, y)).andReturn(new Vec4(x, y, 0.f, 1.f));
-    expect(niftyNode.getId()).andReturn(12);
-    replay(niftyNode);
+    expect(internalNiftyNode.getNiftyNode()).andReturn(niftyNode).anyTimes();
+    expect(internalNiftyNode.getWidth()).andReturn(100);
+    expect(internalNiftyNode.getHeight()).andReturn(100);
+    expect(internalNiftyNode.screenToLocal(x, y)).andReturn(new Vec4(x, y, 0.f, 1.f));
+    expect(internalNiftyNode.getId()).andReturn(12).anyTimes();
+    replay(internalNiftyNode);
   }
 
   private NiftyPointerEvent makePointerEvent(final int x, final int y) {
