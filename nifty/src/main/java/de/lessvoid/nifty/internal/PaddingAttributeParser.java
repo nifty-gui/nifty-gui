e463d4917623e5b4becbabd3d3481472c6947794
void <void256@users.sourceforge.net>
2015-05-05 22:18:08 +0200

    chore: update license copyright to "Nifty GUI Community"
    Nifty GUI is a community effort and therefore the copyright belongs to a
    group of people instead of only a single individual :)

--- nifty/src/main/java/de/lessvoid/nifty/internal/PaddingAttributeParser.java
+++ nifty/src/main/java/de/lessvoid/nifty/internal/PaddingAttributeParser.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2014, Jens Hohmuth
+ * Copyright (c) 2015, Nifty GUI Community
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
