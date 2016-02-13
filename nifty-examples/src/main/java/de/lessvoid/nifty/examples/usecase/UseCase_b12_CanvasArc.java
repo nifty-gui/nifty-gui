d9ba279c123a688a8ba4dae30e7ebb1de54ac895
void256 <void@lessvoid.com>
2016-02-13 17:02:35 +0100

    chore: bring back UseCase_b12_CanvasArc

--- nifty-examples/src/main/java/de/lessvoid/nifty/examples/usecase/UseCase_b12_CanvasArc.java
+++ nifty-examples/src/main/java/de/lessvoid/nifty/examples/usecase/UseCase_b12_CanvasArc.java
@@ -26,87 +26,117 @@
  */
 package de.lessvoid.nifty.examples.usecase;
 
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.NiftyCanvas;
+import de.lessvoid.nifty.NiftyCanvasPainter;
+import de.lessvoid.nifty.NiftyFont;
+import de.lessvoid.nifty.NiftyNodeCallback;
+import de.lessvoid.nifty.node.Horizontal;
+import de.lessvoid.nifty.node.NiftyContentNode;
+import de.lessvoid.nifty.node.Vertical;
+import de.lessvoid.nifty.spi.node.NiftyNode;
+import de.lessvoid.nifty.types.NiftyColor;
+import de.lessvoid.nifty.types.NiftyLineCapType;
+
+import java.io.IOException;
+
+import static de.lessvoid.nifty.node.AlignmentLayoutChildNode.alignmentLayoutChildNode;
+import static de.lessvoid.nifty.node.AlignmentLayoutNode.alignmentLayoutNode;
+import static de.lessvoid.nifty.node.NiftyContentNode.contentNode;
+import static de.lessvoid.nifty.node.NiftyReferenceNode.referenceNode;
+import static de.lessvoid.nifty.node.SizeLayoutNode.fixedSizeLayoutNode;
+import static de.lessvoid.nifty.node.Vertical.Bottom;
+import static de.lessvoid.nifty.node.Vertical.Top;
+
 /**
  * An example how to draw arcs into a NiftyCanvas.
  * @author void
  */
 public class UseCase_b12_CanvasArc {
-  /* FIXME: star redraw method missing.
   private static NiftyFont font;
   private static float v = 1.99f;
 
   public UseCase_b12_CanvasArc(final Nifty nifty) throws IOException {
     font = nifty.createFont("fonts/aurulent-sans-16.fnt");
 
-    nifty.showStatistics(NiftyStatisticsMode.ShowFPS);
-
-    NiftyNode rootNode = nifty.createRootNodeFullscreen(ChildLayout.Horizontal);
-    rootNode.setBackgroundColor(NiftyColor.black());
-
-    NiftyNode butt = rootNode.newChildNode(UnitValue.px(256), UnitValue.px(256));
-    butt.setVAlign(VAlign.center);
-    butt.startAnimatedRedraw(0, 16);
-    butt.addCanvasPainter(new NiftyCanvasPainter() {
+    final NiftyContentNode animatedContentNode = animatedContentNode();
+    nifty.startAnimated(0, 16, animatedContentNode, new NiftyNodeCallback<Float, NiftyNode>() {
       @Override
-      public void paint(final NiftyNode node, final NiftyCanvas canvas) {
-        draw(node, canvas, NiftyLineCapType.Butt);
-      }
-    });
-
-    NiftyNode square = rootNode.newChildNode(UnitValue.px(256), UnitValue.px(256));
-    square.setVAlign(VAlign.center);
-    square.startAnimatedRedraw(0, 16);
-    square.addCanvasPainter(new NiftyCanvasPainter() {
-      @Override
-      public void paint(final NiftyNode node, final NiftyCanvas canvas) {
-        draw(node, canvas, NiftyLineCapType.Square);
-      }
-    });
-
-    NiftyNode round = rootNode.newChildNode(UnitValue.px(256), UnitValue.px(256));
-    round.setVAlign(VAlign.center);
-    round.startAnimatedRedraw(0, 16);
-    round.addCanvasPainter(new NiftyCanvasPainter() {
-      @Override
-      public void paint(final NiftyNode node, final NiftyCanvas canvas) {
-        draw(node, canvas, NiftyLineCapType.Round);
-      }
-    });
-
-    NiftyNode animated = rootNode.newChildNode(UnitValue.px(256), UnitValue.px(256));
-    animated.setVAlign(VAlign.center);
-    animated.startAnimatedRedraw(0, 16);
-    animated.addCanvasPainter(new NiftyCanvasPainter() {
-      @Override
-      public void paint(final NiftyNode node, final NiftyCanvas canvas) {
-        canvas.setLineCap(NiftyLineCapType.Square);
-        canvas.setFillStyle(NiftyColor.blue());
-        canvas.fillRect(0, 0, node.getWidth(), node.getHeight());
-
-        canvas.setTextColor(NiftyColor.white());
-        canvas.text(font, 5, 5, "Animated ;)");
-
-        canvas.beginPath();
-        canvas.arc(128., 128., 100., v * Math.PI, (v + 1.25) * Math.PI);
-        canvas.setLineWidth(24);
-        canvas.setStrokeColor(NiftyColor.white());
-        canvas.stroke();
-
-        canvas.beginPath();
-        canvas.arc(128., 128., 100., v * Math.PI, (v + 1.25) * Math.PI);
-        canvas.setLineWidth(1);
-        canvas.setStrokeColor(NiftyColor.black());
-        canvas.stroke();
-
-        v += 0.001;
+      public void execute(final Float aFloat, final NiftyNode node) {
+        v += 0.005;
         if (v > 2) {
           v -= 2;
         }
+
+        animatedContentNode.redraw();
       }
     });
+
+    nifty
+      .addNode(alignmentLayoutNode())
+        .addNode(alignmentLayoutChildNode(Horizontal.Center, Vertical.Middle))
+          .addNode(fixedSizeLayoutNode(512.f, 512.f))
+            .addNode(alignmentLayoutNode())
+              .addNode(referenceNode("parent"))
+                .addNode(alignmentLayoutChildNode(Horizontal.Left, Top))
+                  .addNode(fixedSizeLayoutNode(256, 256))
+                    .addNode(contentNode().setCanvasPainter(new NiftyCanvasPainter() {
+                      @Override
+                      public void paint(final NiftyContentNode node, final NiftyCanvas canvas) {
+                        draw(node, canvas, NiftyLineCapType.Butt);
+                      }
+                    }))
+              .addAsChildOf("parent")
+                .addNode(alignmentLayoutChildNode(Horizontal.Right, Top))
+                  .addNode(fixedSizeLayoutNode(256, 256))
+                    .addNode(contentNode().setCanvasPainter(new NiftyCanvasPainter() {
+                      @Override
+                      public void paint(final NiftyContentNode node, final NiftyCanvas canvas) {
+                        draw(node, canvas, NiftyLineCapType.Square);
+                    }
+                  }))
+              .addAsChildOf("parent")
+                .addNode(alignmentLayoutChildNode(Horizontal.Left, Bottom))
+                  .addNode(fixedSizeLayoutNode(256, 256))
+                    .addNode(contentNode().setCanvasPainter(new NiftyCanvasPainter() {
+                      @Override
+                      public void paint(final NiftyContentNode node, final NiftyCanvas canvas) {
+                        draw(node, canvas, NiftyLineCapType.Round);
+                      }
+                    }))
+              .addAsChildOf("parent")
+                .addNode(alignmentLayoutChildNode(Horizontal.Right, Bottom))
+                  .addNode(fixedSizeLayoutNode(256, 256))
+                    .addNode(animatedContentNode);
   }
 
-  private static void draw(final NiftyNode node, final NiftyCanvas canvas, final NiftyLineCapType cap) {
+  private NiftyContentNode animatedContentNode() {
+    return contentNode().setCanvasPainter(new NiftyCanvasPainter() {
+        @Override
+        public void paint(final NiftyContentNode node, final NiftyCanvas canvas) {
+          canvas.setLineCap(NiftyLineCapType.Square);
+          canvas.setFillStyle(NiftyColor.blue());
+          canvas.fillRect(0, 0, node.getWidth(), node.getHeight());
+
+          canvas.setTextColor(NiftyColor.white());
+          canvas.text(font, 5, 5, "Animated");
+
+          canvas.beginPath();
+          canvas.arc(128., 128., 100., v * Math.PI, (v + 1.25) * Math.PI);
+          canvas.setLineWidth(24);
+          canvas.setStrokeColor(NiftyColor.white());
+          canvas.stroke();
+
+          canvas.beginPath();
+          canvas.arc(128., 128., 100., v * Math.PI, (v + 1.25) * Math.PI);
+          canvas.setLineWidth(1);
+          canvas.setStrokeColor(NiftyColor.black());
+          canvas.stroke();
+        }
+      });
+  }
+
+  private static void draw(final NiftyContentNode node, final NiftyCanvas canvas, final NiftyLineCapType cap) {
     canvas.setLineCap(cap);
     canvas.setFillStyle(NiftyColor.blue());
     canvas.fillRect(0, 0, node.getWidth(), node.getHeight());
@@ -130,5 +160,4 @@ public class UseCase_b12_CanvasArc {
   public static void main(final String[] args) throws Exception {
     UseCaseRunner.run(UseCase_b12_CanvasArc.class, args);
   }
-  */
 }
