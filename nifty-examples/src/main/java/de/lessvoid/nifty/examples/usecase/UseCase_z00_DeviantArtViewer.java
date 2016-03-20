9b4dbdaced2a7125ee3b0d1c9de23528d1cac642
void256 <void@lessvoid.com>
2016-03-20 16:05:12 +0100

    chore: example UseCase_z00_DeviantArtViewer finished
    example now reads images from a rss feed in a background
    thread and then updates the image on the main thread

--- nifty-examples/src/main/java/de/lessvoid/nifty/examples/usecase/UseCase_z00_DeviantArtViewer.java
+++ nifty-examples/src/main/java/de/lessvoid/nifty/examples/usecase/UseCase_z00_DeviantArtViewer.java
@@ -34,8 +34,28 @@ import de.lessvoid.nifty.NiftyConfiguration;
 import de.lessvoid.nifty.NiftyImage;
 import de.lessvoid.nifty.node.NiftyContentNode;
 import de.lessvoid.nifty.node.NiftyTransformationNode;
+import de.lessvoid.nifty.spi.NiftyRenderDevice.FilterMode;
 import de.lessvoid.nifty.types.NiftyColor;
-import de.lessvoid.nifty.types.NiftyLinearGradient;
+import de.lessvoid.niftyinternal.render.io.ImageLoader;
+import de.lessvoid.niftyinternal.render.io.ImageLoaderImageIO;
+
+import javax.xml.namespace.QName;
+import javax.xml.stream.XMLEventReader;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamException;
+import javax.xml.stream.events.Characters;
+import javax.xml.stream.events.XMLEvent;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.ByteBuffer;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
+import java.util.concurrent.LinkedBlockingQueue;
 
 import static de.lessvoid.nifty.node.AbsoluteLayoutChildNode.absoluteLayoutChildNode;
 import static de.lessvoid.nifty.node.AbsoluteLayoutNode.absoluteLayoutNode;
@@ -51,11 +71,34 @@ import static de.lessvoid.nifty.types.NiftyLinearGradient.linearGradientFromAngl
  * 
  * @author void
  */
-public class UseCase_z00_DeviantArtViewer {
+public class UseCase_z00_DeviantArtViewer implements NiftyCanvasPainter {
+  // we'll load images in a background thread and we'll use this Executor to run the background thread
+  private Executor executor = Executors.newSingleThreadExecutor();
+
+  // this queue is filled in the background thread and is processed in the main thread (where we actually upload the
+  // data as a new NiftyImage)
+  private Queue<RSSImageData> imageQueue = new LinkedBlockingQueue<>();
+
+  // The current NiftyImage we'll display. This is replaced with a new NiftyImage whenever a new one is available.
+  private NiftyImage image;
+
+  // A newly available NiftyImage. This is usually null unless a new one is loaded.
+  private NiftyImage pendingImage;
+
+  // We'll set this to true when it's time to toggle the image.
+  private boolean toggleImage;
+
+  @Override
+  public void paint(final NiftyContentNode node, final NiftyCanvas canvas) {
+    canvas.drawImage(image, node.getWidth()/2 - image.getWidth()/2, node.getHeight()/2 - image.getHeight()/2);
+  }
 
   public UseCase_z00_DeviantArtViewer(final Nifty nifty) {
-    final NiftyImage image = nifty.createNiftyImage("nifty.png");
+    executor.execute(new LoadImagesBackgroundTask());
+
+    image = nifty.createNiftyImage("nifty.png");
     final NiftyTransformationNode transformationNode = transformationNode();
+    final NiftyContentNode contentNode = contentNode().setCanvasPainter(this);
 
     nifty
       .addNode(
@@ -65,23 +108,39 @@ public class UseCase_z00_DeviantArtViewer {
             .addColorStop(1.0, NiftyColor.black())))
         .addNode(contentNode())
           .addNode(absoluteLayoutNode())
-            .addNode(absoluteLayoutChildNode(nifty.getScreenWidth() / 2 - 75, nifty.getScreenHeight() / 2 - 75))
-              .addNode(fixedSizeLayoutNode(150.f, 150.f))
+            .addNode(absoluteLayoutChildNode(nifty.getScreenWidth() / 2 - 400, nifty.getScreenHeight() / 2 - 300))
+              .addNode(fixedSizeLayoutNode(800.f, 600.f))
                 .addNode(transformationNode)
                   .addNode(backgroundFillColor(NiftyColor.fromString("#eeef")))
-                    .addNode(contentNode().setCanvasPainter(new NiftyCanvasPainter() {
-                      @Override
-                      public void paint(final NiftyContentNode node, final NiftyCanvas canvas) {
-                        canvas.drawImage(image, node.getWidth()/2 - image.getWidth()/2, node.getHeight()/2 - image.getHeight()/2);
-                      }
-                    }));
+                    .addNode(contentNode);
     nifty.startTickAnimator(new NiftyCallback<Float>() {
       @Override public void execute(final Float time) {
         transformationNode.setPosX(Math.sin(time) * 512.);
         double scale = (Math.sin((time/2-Math.PI/4+Math.PI/2)*2.0)+1.0)/4.0+0.5;
         transformationNode.setScaleX(scale);
         transformationNode.setScaleY(scale);
-        transformationNode.setAngleZ(Math.sin(time) * 256.);
+        double angleX = Math.sin(time) * 180.0 + 180.0;
+        transformationNode.setAngleX(angleX);
+
+        if (pendingImage == null) {
+          RSSImageData rssImageData = imageQueue.poll();
+          if (rssImageData != null) {
+            pendingImage = rssImageData.createNiftyImage(nifty);
+          }
+        }
+
+        if (angleX >= 269.0 && angleX <= 271.0) {
+          if (!toggleImage) {
+            toggleImage = true;
+            if (pendingImage != null) {
+              image = pendingImage;
+              pendingImage = null;
+              contentNode.redraw();
+            }
+          }
+        } else {
+          toggleImage = false;
+        }
       }
     });
   }
@@ -94,5 +153,150 @@ public class UseCase_z00_DeviantArtViewer {
         .renderBucketWidth(1024)
         .renderBucketHeight(768)
       );
+  }
+
+  /**
+   * RSSImageData transports image data from the background thread to the main thread so that the data can be loaded.
+   */
+  private static class RSSImageData {
+    private final int width;
+    private final int height;
+    private final ByteBuffer byteBuffer;
+
+    private RSSImageData(final int width, final int height, final ByteBuffer byteBuffer) {
+      this.width = width;
+      this.height = height;
+      this.byteBuffer = byteBuffer;
+    }
+
+    public NiftyImage createNiftyImage(final Nifty nifty) {
+      return nifty.createNiftyImage(width, height, byteBuffer, FilterMode.Linear);
+    }
+  }
+
+  /**
+   * This Runnable implementation will parse the RSSFeed, extract and download the images and fill the imageQueue
+   * with imageData.
+   */
+  private class LoadImagesBackgroundTask implements Runnable {
+    @Override
+    public void run() {
+      while (true) {
+        while (!imageQueue.isEmpty()) {
+          try {
+            Thread.sleep(1000);
+          } catch (InterruptedException e) {
+          }
+        }
+        RSSFeedParser parser = new RSSFeedParser("http://backend.deviantart.com/rss.xml?q=special%3Add&type=deviation");
+        for (RSSFeedEntry f : parser.readFeed()) {
+          try {
+            imageQueue.add(f.loadImage());
+          } catch (Exception e) {
+            e.printStackTrace();
+          }
+        }
+      }
+    }
+  }
+
+  /**
+   * A RSSFeedEntry.
+   */
+  private static class RSSFeedEntry {
+    private String title;
+    private String url;
+
+    RSSFeedEntry(final String title, final String url) {
+      this.title = title;
+      this.url = url;
+    }
+
+    @Override
+    public String toString() {
+      return title + ":" + url;
+    }
+
+    public RSSImageData loadImage() throws Exception {
+      URL imageURL = new URL(url);
+      ImageLoader imageLoader = new ImageLoaderImageIO();
+      ByteBuffer byteBuffer = imageLoader.loadAsByteBufferRGBA(imageURL.openStream());
+      return new RSSImageData(imageLoader.getTextureWidth(), imageLoader.getTextureHeight(), byteBuffer);
+    }
+  }
+
+  /**
+   * A brute-force RSSFeedParser.
+   */
+  private static class RSSFeedParser {
+    private final URL url;
+
+    RSSFeedParser(final String feedUrl) {
+      try {
+        this.url = new URL(feedUrl);
+      } catch (MalformedURLException e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    List<RSSFeedEntry> readFeed() {
+      List<RSSFeedEntry> result = new ArrayList<>();
+      try (InputStream in = read()) {
+        XMLEventReader eventReader = XMLInputFactory.newInstance().createXMLEventReader(in);
+        while (eventReader.hasNext()) {
+          XMLEvent event = eventReader.nextEvent();
+          if (event.isStartElement()) {
+            if ("item".equals(event.asStartElement().getName().getLocalPart())) {
+              eventReader.nextEvent();
+              result.add(readItem(eventReader));
+            }
+          }
+        }
+        return result;
+      } catch (Exception e) {
+        throw new RuntimeException(e);
+      }
+    }
+
+    private RSSFeedEntry readItem(final XMLEventReader eventReader) throws XMLStreamException {
+      String title = "";
+      String url = "";
+
+      while (eventReader.hasNext()) {
+        XMLEvent event = eventReader.nextEvent();
+        if (event.isStartElement()) {
+          String namespace = event.asStartElement().getName().getNamespaceURI();
+          if ("http://search.yahoo.com/mrss/".equals(namespace)) {
+            String localPart = event.asStartElement().getName().getLocalPart();
+            if ("title".equals(localPart)) {
+              title = getCharacterData(eventReader);
+            } else if ("content".equals(localPart)) {
+              url = event.asStartElement().getAttributeByName(new QName("", "url")).getValue();
+            }
+          }
+        } else if (event.isEndElement()) {
+          if ("item".equals(event.asEndElement().getName().getLocalPart())) {
+            return new RSSFeedEntry(title, url);
+          }
+        }
+      }
+      throw new XMLStreamException("missing item end event");
+    }
+
+    private String getCharacterData(final XMLEventReader eventReader) throws XMLStreamException {
+      XMLEvent event = eventReader.nextEvent();
+      if (event instanceof Characters) {
+        return event.asCharacters().getData();
+      }
+      return "";
+    }
+
+    private InputStream read() {
+      try {
+        return url.openStream();
+      } catch (IOException e) {
+        throw new RuntimeException(e);
+      }
+    }
   }
 }
