nifty-gui/nifty-gui

commit ae2971db406fe5154e215d028143f5f961c30b85

Add call to run the onclick effect from clickAndRelease method so it's run when keyboard activation events

nifty-core/src/main/java/de/lessvoid/nifty/elements/ElementInteractionClickHandler.java
@@ -180,6 +180,7 @@ public class ElementInteractionClickHandler {
   }
 
   public void clickAndReleaseMouse(@Nonnull final Nifty nifty) {
+    element.startEffectWithoutChildren(EffectEventId.onClick); 
     mouseMethods.clickAndRelease(nifty);
   }
 

commit ae80a39998881ca20f268884ef34988ff2ea00ac

fix: missing import added to ElementInteractionClickHandler
problem was introduced with commit ae2971d

nifty-core/src/main/java/de/lessvoid/nifty/elements/ElementInteractionClickHandler.java
@@ -2,6 +2,7 @@ package de.lessvoid.nifty.elements;
 
 import de.lessvoid.nifty.Nifty;
 import de.lessvoid.nifty.NiftyMethodInvoker;
+import de.lessvoid.nifty.effects.EffectEventId;
 import de.lessvoid.nifty.input.NiftyMouseInputEvent;
 
 import javax.annotation.Nonnull;
@@ -180,7 +181,7 @@ public class ElementInteractionClickHandler {
   }
 
   public void clickAndReleaseMouse(@Nonnull final Nifty nifty) {
-    element.startEffectWithoutChildren(EffectEventId.onClick); 
+    element.startEffectWithoutChildren(EffectEventId.onClick);
     mouseMethods.clickAndRelease(nifty);
   }
 